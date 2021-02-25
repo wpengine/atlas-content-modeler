905ff6e79a3963dbf3ea5d58a8915cd46c723758
John Parris <john.parris@wpengine.com>
2021-02-25 09:22:51 -0500

    Add npm package config.
    Adds React and Parcel dependencies.

---
+++ includes/settings/js/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../scss/index.scss';
+
+function App() {
+	return (
+		<p>This is the App component.</p>
+	);
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
