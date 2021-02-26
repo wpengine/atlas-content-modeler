32f160faf3223884c49b779a7fc53df6346ce212
Ryan Meier <ryan.meier@wpengine.com>
2021-02-26 11:14:35 -0600

    Integrated Codeception acceptance testing

---
+++ tests/acceptance/PageCest.php
@@ -0,0 +1,14 @@
+<?php
+
+class PageCest
+{
+    /**
+     * Ensure the WPEngine Content Modeling page is available.
+     */
+    public function i_can_access_the_content_model_page(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->amOnPage('/wp-admin/admin.php?page=wpe-content-model');
+        $I->see('Content Modeling by WP Engine');
+    }
+}
