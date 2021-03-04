88cf750c175259e26bf147e7f6436e0d4837e55a
John Parris <john.parris@wpengine.com>
2021-03-04 10:06:22 -0500

    Add view for creating model.

--- includes/settings/js/src/App.jsx
+++ includes/settings/js/src/App.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Link,
+	useLocation,
+} from "react-router-dom";
+
+import CreateContentModel from './components/CreateContentModel.jsx';
 
 export default function App() {
 	return (
@@ -188,6 +195,41 @@ export default function App() {
 				</section>
 			</div>
 
-		</div>
+		</div>,
+
+		<Router>
+			<ViewTemplate/>
+		</Router>
+	);
+}
+
+/**
+ * Parses query string and returns value.
+ *
+ * @credit https://reactrouter.com/web/example/query-parameters
+ * @returns {URLSearchParams}
+ */
+function useLocationSearch() {
+	return new URLSearchParams( useLocation().search );
+}
+
+function ViewTemplate() {
+	const query = useLocationSearch();
+	const view = query.get( 'view' );
+
+	if ( view === 'create-model' ) {
+		return <CreateContentModel />
+	}
+
+	if ( view === 'edit-model' ) {
+		return ( <p>edit-model goes here.</p> );
+	}
+
+	return <ViewContentModelsList />
+}
+
+function ViewContentModelsList() {
+	return (
+		<Link className="button button-primary" to="/wp-admin/admin.php?page=wpe-content-model&view=create-model">Add New</Link>
 	);
 }
