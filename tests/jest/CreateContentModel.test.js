185265fd3900cd52ee7dc832eae44e3f7a08beb8
John Parris <john.parris@wpengine.com>
2021-03-05 12:37:43 -0500

    Fix CreateContentModel test.
    * Add jest.config.js with globals export for wp variable.
    * Rename test file to match component name.
    * Import React Router in test file to fix error with rendering Link outside a Router.
    * Update test snapshot

---
+++ tests/jest/CreateContentModel.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CreateContentModel from "../../includes/settings/js/src/components/CreateContentModel";
+
+describe('CreateContentModel tests', () => {
+	const app = renderer.create(
+		<Router>
+			<CreateContentModel/>
+		</Router>
+	);
+
+	let tree = app.toJSON();
+
+	it('Renders a matching snapshot', () => {
+		expect(tree).toMatchSnapshot();
+	});
+});
+
