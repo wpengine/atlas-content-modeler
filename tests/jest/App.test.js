35d40f78e9d433f91be48c07e7ca679a1a5701f9
John Parris <john.parris@wpengine.com>
2021-03-05 12:41:54 -0500

    Rename example test file. Update snapshot.

--- tests/jest/App.test.js
+++ tests/jest/App.test.js
@@ -2,18 +2,14 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../../includes/settings/js/src/App';
 
-describe('Example tests', () => {
-	it('is true that true is true', () => {
-		expect(true).toBe(true);
-	});
-
+describe('App', () => {
 	const app = renderer.create(
 		<App/>
 	);
 
 	let tree = app.toJSON();
 
-	it('React', () => {
+	it('Renders a matching snapshot', () => {
 		expect(tree).toMatchSnapshot();
 	});
 });
