0f6dbcc358b6e91a6914efa8a1cc74935a46427f
Nick Cernis <nick@cern.is>
2021-03-08 18:11:10 +0100

    feat: add fields from the content model page

---
+++ includes/settings/js/src/components/fields/Field.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import TextForm from "./TextForm";
+
+const BooleanForm = () => <p>I will be the boolean form.</p>;
+const NumberForm = () => <p>I will be the number form.</p>;
+
+function Field({type='text', open=false}) {
+	const [activeForm, setActiveForm] = useState(type);
+
+	useEffect(() => {
+		if ( type === 'new' ) {
+			type = 'text';
+			setActiveForm(type)
+			open = true;
+		}
+	}, [])
+
+	const fieldTitle = activeForm.charAt(0).toUpperCase() + activeForm.slice(1);
+
+	return (
+		// TODO: add a key to li when we have unique data to give it.
+		<li>
+			<div>
+				<button onClick={() => setActiveForm('text')}>Text</button>
+				<button onClick={() => setActiveForm('number')}>Number</button>
+				<button onClick={() => setActiveForm('boolean')}>Boolean</button>
+			</div>
+			<h3>New {fieldTitle} Block</h3>
+			{ activeForm === 'text' && <TextForm /> }
+			{ activeForm === 'boolean' && <BooleanForm /> }
+			{ activeForm === 'number' && <NumberForm /> }
+		</li>
+	);
+}
+
+export default Field;
---
+++ includes/settings/js/src/components/fields/TextForm.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import camelcase from "camelcase";
+import {Link} from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+function TextForm() {
+	const { register, handleSubmit, errors, setValue } = useForm();
+	const [ nameCount, setNameCount ] = useState(0);
+
+	function apiAddField() {
+
+	}
+
+	return (
+		<form onSubmit={handleSubmit(apiAddField)}>
+			<div>
+				<label htmlFor="name">Name</label><br/>
+				<p className="help">Singular display name for your content model, e.g. "Rabbit".</p>
+				<input id="name" name="name" placeholder="Name" ref={register({ required: true, maxLength: 50})} onChange={ e => setNameCount(e.target.value.length)} />
+				<p className="limit">{nameCount}/50</p>
+			</div>
+
+			<div>
+				<label htmlFor="fieldSlug">API Identifier</label><br/>
+				<p className="help">Auto-generated and used for API requests.</p>
+				<input id="fieldSlug" name="fieldSlug" ref={register({ required: true, maxLength: 20 })} readOnly="readOnly" />
+			</div>
+
+			<div>
+				<legend>Text Length</legend>
+				<fieldset>
+					<input type="radio" id="short" name="textLength" value="short" ref={register} />
+					<label htmlFor="short">Short text (maximum 50 characters)</label><br/>
+					<input type="radio" id="long" name="textLength" value="long" ref={register} />
+					<label htmlFor="long">Long text (maximum 500 characters)</label>
+				</fieldset>
+			</div>
+
+			<button type="submit" className="primary first">Create</button>
+			<Link to="/wp-admin/admin.php?page=wpe-content-model">
+				<button className="tertiary">Cancel</button>
+			</Link>
+		</form>
+	);
+}
+
+export default TextForm;
