b450cbbc007a9829de825c00708108d587eff432
Nick Cernis <nick@cern.is>
2021-03-09 14:28:37 +0100

    chore: show field in closed state when open=false

--- includes/settings/js/src/components/fields/Field.jsx
+++ includes/settings/js/src/components/fields/Field.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import TextForm from "./TextForm";
+import {AddIcon} from "../icons";
 
 const BooleanForm = () => <p>I will be the boolean form.</p>;
 const NumberForm = () => <p>I will be the number form.</p>;
 
-function Field({type='text', open=false, cancelAction}) {
+function Field({type='text', open=false, cancelAction, id, data={}, addAction, updateAction}) {
 	const [activeForm, setActiveForm] = useState(type);
 
 	useEffect(() => {
@@ -17,16 +18,36 @@ function Field({type='text', open=false, cancelAction}) {
 
 	const fieldTitle = activeForm.charAt(0).toUpperCase() + activeForm.slice(1);
 
+	// Closed fields appear as a row with a summary of info.
+	if (!open) {
+		const typeLabel = data.type.charAt(0).toUpperCase() + data.type.slice(1)
+		return (
+			<>
+				<li key={id}>
+					<span>reorder icon</span>
+					<span>{typeLabel}</span>
+					<span>{data.name}</span>
+					<span>option icon</span>
+				</li>
+				<li className="add-item">
+					<button onClick={addAction} aria-label={`Add a new field below the ${data.name} ${data.type} field`} >
+						<AddIcon/>
+					</button>
+				</li>
+			</>
+		);
+	}
+
+	// Open fields appear as a form to enter or edit data.
 	return (
-		// TODO: add a key to li when we have unique data to give it.
-		<li>
+		<li key={id}>
 			<div>
 				<button onClick={() => setActiveForm('text')}>Text</button>
 				<button onClick={() => setActiveForm('number')}>Number</button>
 				<button onClick={() => setActiveForm('boolean')}>Boolean</button>
 			</div>
 			<h3>New {fieldTitle} Block</h3>
-			{ activeForm === 'text' && <TextForm cancelAction={cancelAction}/> }
+			{ activeForm === 'text' && <TextForm cancelAction={cancelAction} updateAction={updateAction} id={id}/> }
 			{ activeForm === 'boolean' && <BooleanForm /> }
 			{ activeForm === 'number' && <NumberForm /> }
 		</li>
