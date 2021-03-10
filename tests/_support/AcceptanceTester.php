099197058e1ca5f1fc4f050712220277f95041fc
Ryan Meier <ryan.meier@wpengine.com>
2021-03-10 14:40:05 -0600

    Added end-2-end tests for adding a text field to a content model.

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -19,4 +19,59 @@
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
+
+    /**
+     * Visit WPEngine Content Edit Model page.
+     *
+     * @param string $id The content model id.
+     */
+    public function amOnWPEngineEditContentModelPage($id)
+    {
+        $this->amOnWPEngineContentModelPage("&view=edit-model&id={$id}");
+    }
+
+    /**
+     * Visit WPEngine Create Content Model page.
+     */
+    public function amOnWPEngineCreateContentModelPage()
+    {
+        $this->amOnWPEngineContentModelPage('&view=create-model');
+    }
+
+    /**
+     * Visit WPEngine Content Model page.
+     *
+     * @param string $params Optional query parameter string.
+     */
+    public function amOnWPEngineContentModelPage($params = '')
+    {
+        $path = '/wp-admin/admin.php?page=wpe-content-model';
+
+        if ( $params ) {
+            $path .= $params;
+        }
+
+        $this->amOnPage($path);
+    }
+
+    /**
+     * Set a wpe_headless setting value.
+     *
+     * @param string $name  The wpe_headless setting name.
+     * @param string $value The wpe_headless setting value.
+     */
+    public function haveContentModel($singular, $plural, $description = '')
+    {
+        $this->amOnPage('/wp-admin/admin.php?page=wpe-content-model&view=create-model');
+        $this->wait(1);
+
+        $this->fillField(['name' => 'singular'], $singular);
+        $this->fillField(['name' => 'plural'], $plural);
+
+        if ($description) {
+            $this->fillField(['name' => 'description'], $description);
+        }
+
+        $this->click('.card-content button.primary');
+    }
 }
--- tests/acceptance/CreateContentModelCest.php
+++ tests/acceptance/CreateContentModelCest.php
@@ -8,7 +8,7 @@ class CreateContentModelCest
 	public function i_see_a_message_when_i_have_no_content_models(AcceptanceTester $I)
 	{
 		$I->loginAsAdmin();
-    	$I->amOnPage('/wp-admin/admin.php?page=wpe-content-model');
+    	$I->amOnWPEngineContentModelPage();
     	$I->wait(1);
 		$I->see('You have no Content Models. It might be a good idea to create one now.');
 	}
@@ -19,16 +19,16 @@ class CreateContentModelCest
     public function i_can_create_a_content_model(AcceptanceTester $I)
     {
     	$I->loginAsAdmin();
-        $I->amOnPage('/wp-admin/admin.php?page=wpe-content-model&view=create-model');
+        $I->amOnWPEngineCreateContentModelPage();
         $I->wait(1);
 
         $I->fillField(['name' => 'singular'], 'Candy');
         $I->fillField(['name' => 'plural'], 'Candies');
         $I->fillField(['name' => 'description'], 'My candy content model');
-        $I->see('22/250', 'p.limit');
+        $I->see('22/250', 'span.count');
         $I->click('.card-content button.primary');
 
-        $I->amOnPage('/wp-admin/admin.php?page=wpe-content-model');
+        $I->amOnWPEngineContentModelPage();
         $I->wait(1);
         $I->see('Candies', '.model-list');
         $I->see('My candy content model', '.model-list');
