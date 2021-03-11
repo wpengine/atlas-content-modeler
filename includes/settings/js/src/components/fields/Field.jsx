bde7b640b4010a5126a3f466f2c207f94d5d4465
Ryan Meier <ryan.meier@wpengine.com>
2021-03-11 14:18:40 -0600

    Added boolean field to content model fields

--- includes/settings/js/src/components/fields/Field.jsx
+++ includes/settings/js/src/components/fields/Field.jsx
@@ -2,9 +2,8 @@ import React, {useState, useEffect} from 'react';
 import MediaForm from "./MediaForm";
 import TextForm from "./TextForm";
 import NumberForm from "./NumberForm";
+import BooleanForm from "./BooleanForm";
 import {AddIcon, OptionsIcon, ReorderIcon, TextIcon, BooleanIcon, NumberIcon, MediaIcon} from "../icons";
-
-const BooleanForm = () => <p>Boolean form coming soon.</p>;
 
 function Field({type='text', position, open=false, cancelAction, id, data={}, addAction, updateAction, positionAfter}) {
 	const [activeForm, setActiveForm] = useState(type);
@@ -112,7 +111,7 @@ function Field({type='text', position, open=false, cancelAction, id, data={}, ad
 			<div className="field-form">
 				<h3>New {fieldTitle} Field</h3>
 				{ activeForm === 'text' && <TextForm cancelAction={cancelAction} updateAction={updateAction} id={id} position={position}/> }
-				{ activeForm === 'boolean' && <BooleanForm /> }
+				{ activeForm === 'boolean' && <BooleanForm cancelAction={cancelAction} updateAction={updateAction} id={id} position={position}/> }
 				{ activeForm === 'number' && <NumberForm cancelAction={cancelAction} updateAction={updateAction} id={id} position={position}/> }
 				{ activeForm === 'media' && <MediaForm cancelAction={cancelAction} updateAction={updateAction} id={id} position={position}/> }
 			</div>
