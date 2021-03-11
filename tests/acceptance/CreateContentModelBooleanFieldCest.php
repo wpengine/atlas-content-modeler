77d4afcce52cfe544f2f9e38d97ba7af23cbb831
Ryan Meier <ryan.meier@wpengine.com>
2021-03-11 14:18:59 -0600

    Added end-2-end test to add a boolean field to a content model

---
+++ tests/acceptance/CreateContentModelBooleanFieldCest.php
@@ -0,0 +1,28 @@
+<?php
+
+class CreateContentModelBooleanFieldCest
+{
+    /**
+     * Ensure a user can add a boolean field to the model and see it within the list.
+     */
+    public function i_can_create_a_content_model_boolean_field(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->haveContentModel('Candy', 'Candies');
+        $I->amOnWPEngineEditContentModelPage('candies');
+        $I->wait(1);
+
+        $I->click('li.add-item button');
+        $I->wait(1);
+
+        $I->click('Boolean', '.field-list .open-field .field-buttons');
+        $I->fillField(['name' => 'name'], 'Accept Terms');
+        $I->see('12/50', 'span.count');
+        $I->seeInField('#slug','acceptTerms');
+        $I->click('.open-field button.primary');
+        $I->wait(1);
+
+        $I->see('Boolean', '.field-list span.type');
+        $I->see('Accept Terms', '.field-list span.widest');
+    }
+}
