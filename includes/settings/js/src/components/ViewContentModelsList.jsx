1ec79c4c27693ec417536bdac285f42307db6007
John Parris <john.parris@wpengine.com>
2021-03-17 11:06:04 -0400

    Use Icon component instead of svg

--- includes/settings/js/src/components/ViewContentModelsList.jsx
+++ includes/settings/js/src/components/ViewContentModelsList.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "react-modal";
 import { ModelsContext } from "../ModelsContext";
+import Icon from "./icons";
 
 const { apiFetch } = wp;
 
@@ -123,27 +124,7 @@ const ContentModelDropdown = ({model}) => {
 					setDropdownOpen(!dropdownOpen);
 				} }
 			>
-				<svg
-					className="options"
-					width="16"
-					height="4"
-					viewBox="0 0 16 4"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M3.79995 1.99995C3.79995 2.99406 2.99406 3.79995 1.99995 3.79995C1.00584 3.79995 0.199951 2.99406 0.199951 1.99995C0.199951 1.00584 1.00584 0.199951 1.99995 0.199951C2.99406 0.199951 3.79995 1.00584 3.79995 1.99995Z"
-						fill="#002838"
-					/>
-					<path
-						d="M9.79995 1.99995C9.79995 2.99406 8.99406 3.79995 7.99995 3.79995C7.00584 3.79995 6.19995 2.99406 6.19995 1.99995C6.19995 1.00584 7.00584 0.199951 7.99995 0.199951C8.99406 0.199951 9.79995 1.00584 9.79995 1.99995Z"
-						fill="#002838"
-					/>
-					<path
-						d="M14 3.79995C14.9941 3.79995 15.8 2.99406 15.8 1.99995C15.8 1.00584 14.9941 0.199951 14 0.199951C13.0058 0.199951 12.2 1.00584 12.2 1.99995C12.2 2.99406 13.0058 3.79995 14 3.79995Z"
-						fill="#002838"
-					/>
-				</svg>
+				<Icon type="options" />
 			</button>
 			<div className={`dropdown-content ${dropdownOpen ? '' : 'hidden' }`}>
 				<a>Edit</a>
