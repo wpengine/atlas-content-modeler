f96e362131460d1dd47c7908b6ad94da04ab09a1
Nick Cernis <nick@cern.is>
2021-03-17 13:41:41 +0100

    design: add Rich Text icon

---
+++ includes/settings/js/src/components/icons/RichTextIcon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RichTextIcon = () => {
+	return (
+		<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M22 0H0V2H22V0Z" fill="#002838"/>
+			<path d="M0 5H12V7H0V5Z" fill="#002838"/>
+			<path d="M0 10H16V12H0V10Z" fill="#002838"/>
+			<path d="M8 15H0V17H8V15Z" fill="#002838"/>
+			<path d="M0 20H13V22H0V20Z" fill="#002838"/>
+		</svg>
+	);
+};
+
+export default RichTextIcon;
