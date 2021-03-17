0c6ca4b5655e4287a46098d537de966e34d6742e
Nick Cernis <nick@cern.is>
2021-03-17 09:50:10 +0100

    chore: default to stored data when editing fields

--- includes/settings/js/src/components/fields/MediaFields.jsx
+++ includes/settings/js/src/components/fields/MediaFields.jsx
@@ -3,15 +3,36 @@
  */
 import React from 'react';
 
-const MediaFields = ({register}) => {
+const MediaFields = ({register, data}) => {
 	return (
 		<div className="field">
 			<legend>Upload Type</legend>
 			<fieldset>
-				<input type="radio" id="single" name="uploadType" value="single" ref={register} defaultChecked />
-				<label className="radio" htmlFor="single">One file</label><br/>
-				<input type="radio" id="multi" name="uploadType" value="multi" ref={register} />
-				<label className="radio" htmlFor="multi">Many files</label>
+				<input
+					type="radio"
+					id="single"
+					name="uploadType"
+					value="single"
+					ref={register}
+					defaultChecked={
+						data?.uploadType === "single" || typeof data?.uploadType === "undefined"
+					}
+				/>
+				<label className="radio" htmlFor="single">
+					One file
+				</label>
+				<br />
+				<input
+					type="radio"
+					id="multi"
+					name="uploadType"
+					value="multi"
+					ref={register}
+					defaultChecked={data?.uploadType === "multi"}
+				/>
+				<label className="radio" htmlFor="multi">
+					Many files
+				</label>
 			</fieldset>
 		</div>
 	);
--- includes/settings/js/src/components/fields/TextFields.jsx
+++ includes/settings/js/src/components/fields/TextFields.jsx
@@ -3,15 +3,36 @@
  */
 import React from 'react';
 
-const TextFields = ({register}) => {
+const TextFields = ({register, data}) => {
 	return (
 		<div className="field">
 			<legend>Text Length</legend>
 			<fieldset>
-				<input type="radio" id="short" name="textLength" value="short" ref={register} defaultChecked />
-				<label className="radio" htmlFor="short">Short text (maximum 50 characters)</label><br/>
-				<input type="radio" id="long" name="textLength" value="long" ref={register} />
-				<label className="radio" htmlFor="long">Long text (maximum 500 characters)</label>
+				<input
+					type="radio"
+					id="short"
+					name="textLength"
+					value="short"
+					ref={register}
+					defaultChecked={
+						data?.textLength === "short" || typeof data?.textLength === "undefined"
+					}
+				/>
+				<label className="radio" htmlFor="short">
+					Short text (maximum 50 characters)
+				</label>
+				<br />
+				<input
+					type="radio"
+					id="long"
+					name="textLength"
+					value="long"
+					ref={register}
+					defaultChecked={data?.textLength === "long"}
+				/>
+				<label className="radio" htmlFor="long">
+					Long text (maximum 500 characters)
+				</label>
 			</fieldset>
 		</div>
 	);
