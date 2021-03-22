2617784d078ab457c81036cef8db91994e84a396
Nick Cernis <nick@cern.is>
2021-03-22 11:34:31 +0100

    chore: formatting

--- includes/settings/js/src/reducer.js
+++ includes/settings/js/src/reducer.js
@@ -3,28 +3,28 @@
  */
 export function reducer(state, action) {
 	switch (action.type) {
-		case 'updateModels':
+		case "updateModels":
 			return action.data;
-		case 'addModel':
+		case "addModel":
 			return {
 				...state,
 				[action.data.postTypeSlug]: action.data,
 			};
-		case 'removeModel':
+		case "removeModel":
 			const { [action.slug]: deleted, ...otherModels } = state;
 			return otherModels;
-		case 'addField':
-			return {todo: 'fill me in'}
-		case 'openField':
-			return {todo: 'fill me in'}
-		case 'closeField':
-			return {todo: 'fill me in'}
-		case 'updateField':
-			return {todo: 'fill me in'}
-		case 'removeField':
-			return {todo: 'fill me in'}
-		case 'swapFieldPositions':
-			return {todo: 'fill me in'}
+		case "addField":
+			return { todo: "fill me in" };
+		case "openField":
+			return { todo: "fill me in" };
+		case "closeField":
+			return { todo: "fill me in" };
+		case "updateField":
+			return { todo: "fill me in" };
+		case "removeField":
+			return { todo: "fill me in" };
+		case "swapFieldPositions":
+			return { todo: "fill me in" };
 		default:
 			throw new Error(`${action.type} not found`);
 	}
