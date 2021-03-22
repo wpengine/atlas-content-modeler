2780d00b704f6b43e137381bd15dad4238b96fd6
Nick Cernis <nick@cern.is>
2021-03-22 16:54:06 +0100

    chore: formatting

--- includes/settings/js/src/reducer.js
+++ includes/settings/js/src/reducer.js
@@ -15,33 +15,48 @@ export function reducer(state, action) {
 			return otherModels;
 		case "addField":
 			const newId = Date.now();
-			state[action.model]['fields'] = {
-				...state[action.model]['fields'],
-				[newId]: { id: newId, type: 'text', open: true, position: action.position}
+			state[action.model]["fields"] = {
+				...state[action.model]["fields"],
+				[newId]: {
+					id: newId,
+					type: "text",
+					open: true,
+					position: action.position,
+				},
 			};
-			return {...state};
+			return { ...state };
 		case "openField":
-			state[action.model]['fields'][action.id].open = true;
-			state[action.model]['fields'][action.id].editing = true;
-			return {...state};
+			state[action.model]["fields"][action.id].open = true;
+			state[action.model]["fields"][action.id].editing = true;
+			return { ...state };
 		case "closeField":
-			state[action.model]['fields'][action.id].open = false;
-			state[action.model]['fields'][action.id].editing = false;
-			return {...state};
+			state[action.model]["fields"][action.id].open = false;
+			state[action.model]["fields"][action.id].editing = false;
+			return { ...state };
 		case "updateField":
-			state[action.model]['fields'][action.data.id] = { ...action.data, open: false, editing: false };
-			return {...state};
+			state[action.model]["fields"][action.data.id] = {
+				...action.data,
+				open: false,
+				editing: false,
+			};
+			return { ...state };
 		case "removeField":
-			delete state[action.model]['fields'][action.id];
-			return {...state};
+			delete state[action.model]["fields"][action.id];
+			return { ...state };
 		case "swapFieldPositions":
-			const fields = state[action.model]['fields'];
-			state[action.model]['fields'] = {
+			const fields = state[action.model]["fields"];
+			state[action.model]["fields"] = {
 				...fields,
-				[action.id1]: {...fields[action.id1], position: fields[action.id2].position },
-				[action.id2]: {...fields[action.id2], position: fields[action.id1].position },
-			}
-			return {...state};
+				[action.id1]: {
+					...fields[action.id1],
+					position: fields[action.id2].position,
+				},
+				[action.id2]: {
+					...fields[action.id2],
+					position: fields[action.id1].position,
+				},
+			};
+			return { ...state };
 		default:
 			throw new Error(`${action.type} not found`);
 	}
