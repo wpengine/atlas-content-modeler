8d83e745f64f53d36d6ad8ddec3771ba236764b0
Nick Cernis <nick@cern.is>
2021-03-22 11:26:13 +0100

    chore: add reducer file and model reducers

---
+++ includes/settings/js/src/reducer.js
@@ -0,0 +1,31 @@
+/**
+ * Actions to update global content model state.
+ */
+export function reducer(state, action) {
+	switch (action.type) {
+		case 'updateModels':
+			return action.data;
+		case 'addModel':
+			return {
+				...state,
+				[action.data.postTypeSlug]: action.data,
+			};
+		case 'removeModel':
+			const { [action.slug]: deleted, ...otherModels } = state;
+			return otherModels;
+		case 'addField':
+			return {todo: 'fill me in'}
+		case 'openField':
+			return {todo: 'fill me in'}
+		case 'closeField':
+			return {todo: 'fill me in'}
+		case 'updateField':
+			return {todo: 'fill me in'}
+		case 'removeField':
+			return {todo: 'fill me in'}
+		case 'swapFieldPositions':
+			return {todo: 'fill me in'}
+		default:
+			throw new Error(`${action.type} not found`);
+	}
+}
