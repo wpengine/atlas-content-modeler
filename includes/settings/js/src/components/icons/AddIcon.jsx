eec9eda2fec102023faed5ff60da775caeed87a5
Nick Cernis <nick@cern.is>
2021-03-23 18:44:45 +0100

    design: manually adjust button size
    Had to crop canvas to 32px manually so that the
    SVG would be centered in the button.
    
    Figma generates SVGs with a large white space
    around them and I couldn't find a way to adjust
    this. It doesn't matter for most SVGs but did
    here, where precise placement was important.

--- includes/settings/js/src/components/icons/AddIcon.jsx
+++ includes/settings/js/src/components/icons/AddIcon.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 
 function AddIcon() {
 	return (
-		<svg className="add" width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<g filter="url(#filter0_d)">
-				<circle cx="36" cy="31" r="16" fill="#7E5CEF"/>
-				<path d="M37.3335 37.3335C37.3335 37.5176 37.1843 37.6668 37.0002 37.6668H35.0002C34.8161 37.6668 34.6668 37.5176 34.6668 37.3335V32.3335H29.6668C29.4827 32.3335 29.3335 32.1843 29.3335 32.0002V30.0002C29.3335 29.8161 29.4827 29.6668 29.6668 29.6668H34.6668V24.6668C34.6668 24.4827 34.8161 24.3335 35.0002 24.3335H37.0002C37.1843 24.3335 37.3335 24.4827 37.3335 24.6668V29.6668H42.3335C42.5176 29.6668 42.6668 29.8161 42.6668 30.0002V32.0002C42.6668 32.1843 42.5176 32.3335 42.3335 32.3335H37.3335V37.3335Z" fill="white"/>
+		<svg className="add" width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">
+			<g transform="translate(-20 -15)">
+				<circle cx="36" cy="31" r="16" fill="#7e5cef"/><path d="M37.333 37.333a.334.334 0 01-.333.334h-2a.334.334 0 01-.333-.334v-5h-5a.334.334 0 01-.334-.333v-2c0-.184.15-.333.334-.333h5v-5c0-.184.149-.334.333-.334h2c.184 0 .333.15.333.334v5h5c.184 0 .334.149.334.333v2c0 .184-.15.333-.334.333h-5v5z" fill="#fff" fillRule="nonzero"/>
 			</g>
-			<defs>
-				<filter id="filter0_d" x="0" y="0" width="72" height="72" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-					<feFlood floodOpacity="0" result="BackgroundImageFix"/>
-					<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-					<feOffset dy="5"/>
-					<feGaussianBlur stdDeviation="10"/>
-					<feColorMatrix type="matrix" values="0 0 0 0 0.266667 0 0 0 0 0.266667 0 0 0 0 0.266667 0 0 0 0.15 0"/>
-					<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-					<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-				</filter>
-			</defs>
 		</svg>
 	);
 }
