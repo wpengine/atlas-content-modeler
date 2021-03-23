97f28675a2d9d656d817fd714a728874a15dfc93
Nick Cernis <nick@cern.is>
2021-03-23 18:26:11 +0100

    design: add larger plus icon

--- includes/settings/js/src/components/icons/AddIcon.jsx
+++ includes/settings/js/src/components/icons/AddIcon.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function AddIcon() {
 	return (
-		<svg className="add" width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<svg className="add" width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<g filter="url(#filter0_d)">
-				<circle cx="32" cy="27" r="12" fill="#7E5CEF"/>
-				<path d="M33.3333 33.3333C33.3333 33.5173 33.1841 33.6666 33 33.6666H31C30.8159 33.6666 30.6666 33.5173 30.6666 33.3333V28.3333H25.6666C25.4826 28.3333 25.3333 28.184 25.3333 27.9999V25.9999C25.3333 25.8158 25.4826 25.6666 25.6666 25.6666H30.6666V20.6666C30.6666 20.4825 30.8159 20.3333 31 20.3333H33C33.1841 20.3333 33.3333 20.4825 33.3333 20.6666V25.6666H38.3333C38.5174 25.6666 38.6666 25.8158 38.6666 25.9999V27.9999C38.6666 28.184 38.5174 28.3333 38.3333 28.3333H33.3333V33.3333Z" fill="white"/>
+				<circle cx="36" cy="31" r="16" fill="#7E5CEF"/>
+				<path d="M37.3335 37.3335C37.3335 37.5176 37.1843 37.6668 37.0002 37.6668H35.0002C34.8161 37.6668 34.6668 37.5176 34.6668 37.3335V32.3335H29.6668C29.4827 32.3335 29.3335 32.1843 29.3335 32.0002V30.0002C29.3335 29.8161 29.4827 29.6668 29.6668 29.6668H34.6668V24.6668C34.6668 24.4827 34.8161 24.3335 35.0002 24.3335H37.0002C37.1843 24.3335 37.3335 24.4827 37.3335 24.6668V29.6668H42.3335C42.5176 29.6668 42.6668 29.8161 42.6668 30.0002V32.0002C42.6668 32.1843 42.5176 32.3335 42.3335 32.3335H37.3335V37.3335Z" fill="white"/>
 			</g>
 			<defs>
-				<filter id="filter0_d" x="0" y="0" width="64" height="64" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+				<filter id="filter0_d" x="0" y="0" width="72" height="72" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
 					<feFlood floodOpacity="0" result="BackgroundImageFix"/>
 					<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
 					<feOffset dy="5"/>
