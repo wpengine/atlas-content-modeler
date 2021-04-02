b5fb92c4928bf6d0e71de0195826fe0f789f805c
Nick Cernis <nick@cern.is>
2021-04-02 18:05:39 +0200

    refactor: flatten nesting

--- includes/settings/js/src/components/fields/eventHandlers.js
+++ includes/settings/js/src/components/fields/eventHandlers.js
@@ -13,29 +13,33 @@ const { apiFetch } = wp;
 export function handleDragEnd(event, fields, modelSlug, dispatch) {
 	const {active, over} = event;
 
-	if (active.id !== over.id) {
-		const oldIndex = fields.indexOf(active.id);
-		const newIndex = fields.indexOf(over.id);
-		const newOrder = arrayMove(fields, oldIndex, newIndex);
-
-		let pos = 0;
-		const idsAndNewPositions = newOrder.reduce((result, id) => {
-			result[id] = {position: pos};
-			pos += 10000;
-			return result;
-		}, {});
-
-		dispatch({type: 'reorderFields', positions: idsAndNewPositions, model: modelSlug});
-
-		const updatePositions = async () => {
-			await apiFetch({
-				path: `/wpe/content-model-fields/${modelSlug}`,
-				method: "PATCH",
-				_wpnonce: wpApiSettings.nonce,
-				data: {fields: idsAndNewPositions},
-			});
-		};
-
-		updatePositions().catch(err => console.error(err));
+	// Skips updates if field order has not changed.
+	if (active.id === over.id) {
+		return;
 	}
+
+	const oldIndex = fields.indexOf(active.id);
+	const newIndex = fields.indexOf(over.id);
+	const newOrder = arrayMove(fields, oldIndex, newIndex);
+	const fieldPositionGap = 10000;
+
+	let pos = 0;
+	const idsAndNewPositions = newOrder.reduce((result, id) => {
+		result[id] = {position: pos};
+		pos += fieldPositionGap;
+		return result;
+	}, {});
+
+	dispatch({type: 'reorderFields', positions: idsAndNewPositions, model: modelSlug});
+
+	const updatePositions = async () => {
+		await apiFetch({
+			path: `/wpe/content-model-fields/${modelSlug}`,
+			method: "PATCH",
+			_wpnonce: wpApiSettings.nonce,
+			data: {fields: idsAndNewPositions},
+		});
+	};
+
+	updatePositions().catch(err => console.error(err));
 }
