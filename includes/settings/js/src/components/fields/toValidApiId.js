8cd6f7aef34f75a99246ec0ecc614e2b39cfee39
Nick Cernis <nick@cern.is>
2021-04-05 16:17:26 +0200

    fix: add toValidApiId for legal API IDs

---
+++ includes/settings/js/src/components/fields/toValidApiId.js
@@ -0,0 +1,26 @@
+/**
+ * Converts a string to a valid GraphQL name matching `[_A-Za-z][_0-9A-Za-z]`.
+ * Also turns `input with spaces` into the camel cased `inputWithSpaces`.
+ *
+ * @see https://spec.graphql.org/June2018/#sec-Names
+ * @param {string} value The input value to convert to a valid GraphQL ID.
+ * @return {string} The valid API ID.
+ */
+export function toValidApiId(value) {
+	// Strip all characters not in the range [_0-9A-Za-z ].
+	// Preserve spaces for now to turn “camel case” into “camelCase” later.
+	value = value.replace(/[^_0-9A-Za-z ]/g, "");
+
+	// Strip leading numbers.
+	value = value.replace(/^[0-9]+/, "");
+
+	// Strip leading white spaces.
+	// So that `toValidApiId(' name')` gives `name` and not `Name`.
+	value = value.trimStart();
+
+	// Turn [space character] into the uppercase character.
+	value = value.replace(/ ([a-z])/g, (match) => match.trim().toUpperCase());
+
+	// Return with final stray spaces removed.
+	return value.replace(/\s/g, "");
+}
---
+++ includes/settings/js/src/components/fields/toValidApiId.test.js
@@ -0,0 +1,20 @@
+import { toValidApiId } from "./toValidApiId";
+
+describe("toValidApiId", () => {
+	const cases = [
+		[",,,#!!!strip_punctuation???$...", "strip_punctuation"],
+		["⚠️⚠️⚠️strip_emoji🐇🐇🐇", "strip_emoji"],
+		["1strip_leading_number", "strip_leading_number"],
+		["123strip_leading_numbers", "strip_leading_numbers"],
+		["     trim_white_space     ", "trim_white_space"],
+		["camel case    white    spaces", "camelCaseWhiteSpaces"],
+		[
+			"allow numbers after 1st character123",
+			"allowNumbersAfter1stCharacter123",
+		],
+	];
+
+	test.each(cases)("toValidApiId(%s) should be %s", (input, expected) => {
+		expect(toValidApiId(input)).toEqual(expected);
+	});
+});
