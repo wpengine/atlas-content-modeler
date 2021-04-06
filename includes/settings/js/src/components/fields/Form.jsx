897449df71804e3d20b70363cbc867e096d616d0
Nick Cernis <nick@cern.is>
2021-04-06 13:31:30 +0200

    [BH-922] Fix API ID field to generate correct IDs from field title and model plural name (#48)
    * fix: add toValidApiId for legal API IDs
    
    * chore: add custom hook to link name and API fields
    
    * chore: amend forms to use API ID generator
    
    * chore: remove camelcase dependency
    
    * chore: bump maxLength of API field to 50
    
    To match the input field max. 50 is still an
    arbitrary limit, but this will reduce the chance
    of forms being rejected when an API ID has been
    generated from a title exceeds 20 characters.
    
    * chore: lower case initial capitals
    
    So that IDs start with a lowercase letter. Not
    required by the GraphQL spec but consistent with
    common usage.
    
    * test: check leading underscores are permitted
    
    * test: check accented characters are stripped
    
    * refactor: remove trimStart call
    
    This scenario is now covered by making the initial
    letter lowercase.
    
    * chore: remove old comment
    
    * fix: revert maxLength for post slug to 20
    
    There's a hard-limit in register_post_type:
    
    if ( empty( $post_type ) || strlen( $post_type ) > 20 ) {
        _doing_it_wrong( __FUNCTION__, __( 'Post type names must be between 1 and 20 characters in length.' ), '4.2.0' );
        return new WP_Error( 'post_type_length_invalid', __( 'Post type names must be between 1 and 20 characters in length.' ) );
    }
    
    Props @mindctrl.

--- includes/settings/js/src/components/fields/Form.jsx
+++ includes/settings/js/src/components/fields/Form.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useContext} from 'react';
-import camelcase from "camelcase";
 import { useForm } from "react-hook-form";
 import { useLocationSearch } from "../../utils";
 import Icon from "../icons";
@@ -8,6 +7,7 @@ import NumberFields from "./NumberFields";
 import MediaFields from "./MediaFields";
 import supportedFields from "./supportedFields";
 import {ModelsContext} from "../../ModelsContext";
+import {useApiIdGenerator} from "./useApiIdGenerator";
 
 const { apiFetch } = wp;
 
@@ -24,6 +24,11 @@ function Form({id, position, type, editing, storedData, parent}) {
 	const query = useLocationSearch();
 	const model = query.get('id');
 	const ExtraFields = extraFields[type] ?? null;
+	const { setApiIdGeneratorInput, apiIdFieldAttributes } = useApiIdGenerator({
+		setValue,
+		editing,
+		storedData,
+	});
 
 	function apiAddField(data) {
 		apiFetch( {
@@ -74,7 +79,7 @@ function Form({id, position, type, editing, storedData, parent}) {
 							placeholder="Name"
 							ref={register({ required: true, maxLength: 50})}
 							onChange={ e => {
-								setValue( 'slug', camelcase( e.target.value ) );
+								setApiIdGeneratorInput(e.target.value);
 								setNameCount(e.target.value.length);
 								clearErrors('slug');
 							} } />
@@ -106,14 +111,14 @@ function Form({id, position, type, editing, storedData, parent}) {
 							name="slug"
 							defaultValue={storedData?.slug}
 							className={errors.slug && 'alert'}
-							ref={register({ required: true, maxLength: 20 })}
-							readOnly="readOnly" />
+							ref={register({ required: true, maxLength: 50 })}
+							{...apiIdFieldAttributes}/>
 						<p className="field-messages">
 							{errors.slug && errors.slug.type === "required" && (
 								<span className="error"><Icon type="error" /><span role="alert">This field is required</span></span>
 							)}
 							{errors.slug && errors.slug.type === "maxLength" && (
-								<span className="error"><Icon type="error" /><span role="alert">Exceeds max length of 20.</span></span>
+								<span className="error"><Icon type="error" /><span role="alert">Exceeds max length of 50.</span></span>
 							)}
 							{errors.slug && errors.slug.type === "idExists" && (
 								<span className="error"><Icon type="error" /><span role="alert">Another field in this model has the same API identifier.</span></span>
---
+++ includes/settings/js/src/components/fields/toValidApiId.js
@@ -0,0 +1,27 @@
+/**
+ * Converts a string to a valid GraphQL name matching `[_A-Za-z][_0-9A-Za-z]`.
+ * Also turns `input with spaces` into the camel cased `inputWithSpaces`.
+ *
+ * @see https://spec.graphql.org/June2018/#sec-Names
+ * @param {string} value The input value to convert to a valid GraphQL ID.
+ * @return {string} The valid API ID.
+ */
+export function toValidApiId(value) {
+	// Strip all characters not in the range [_0-9A-Za-z ].
+	// Preserve spaces for now to turn “camel case” into “camelCase” later.
+	value = value.replace(/[^_0-9A-Za-z ]/g, "");
+
+	// Strip leading numbers.
+	value = value.replace(/^[0-9]+/, "");
+
+	// Turn [space character] into the uppercase character.
+	value = value.replace(/ ([a-z])/g, (match) => match.trim().toUpperCase());
+
+	// Strip any remaining spaces.
+	value = value.replace(/\s/g, "");
+
+	// Lowercase the first letter. Not required by the GraphQL spec, but consistent with common usage.
+	value = value.replace(/^[A-Z]/g, (match) => match.toLowerCase());
+
+	return value;
+}
