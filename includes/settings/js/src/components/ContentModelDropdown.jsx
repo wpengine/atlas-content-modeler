d1c4f79f0b85881f8d25b569fddfee27bb625dab
Nick Cernis <nick@cern.is>
2021-04-08 19:13:40 +0200

    feat: add model options dropdown to edit model screen (#53)
    * refactor: extract ContentModelDropdown
    
    So it can be used on the EditContentModel view
    as well as the current ViewContenModelsList view.
    
    * feat: add model options dropdown to the edit view
    
    * fix: return to models index when deleting a model
    
    The EditContentModel view will be blank and throw
    errors if the user does not return to the
    ViewContentModelList screen once the model they
    were on is deleted.
    
    * refactor: move defaultValues to field attributes
    
    This forces them to update when the edit form
    has been submitted.
    
    Before this change, editing a model then opening
    the edit form again would show stale data from
    before the edit.
    
    * design: fix view content model option button size
    
    * design: center placement of dropdown content
    
    Prevents dropdown content appearing off the page
    on the Edit Content Screen.
    
    * design: add hover states for dropdown links
    
    * fix: close dropdowns when they lose focus
    
    To address feedback from JC.
    
    * design: hide scrollbars around modal overlay
    
    To prevent show-through from the WP admin page to
    the right and bottom of the modal overlay on
    tall modals and narrow screen heights.
    
    This does not prevent scrolling — it just hides
    the doubled scrollbars.

---
+++ includes/settings/js/src/components/ContentModelDropdown.jsx
@@ -0,0 +1,127 @@
+import React, {useContext, useState} from "react";
+import {ModelsContext} from "../ModelsContext";
+import Icon from "./icons";
+import Modal from "react-modal";
+import {EditModelModal} from "./EditModelModal";
+import {useHistory} from "react-router-dom";
+import {maybeCloseDropdown} from "../utils";
+
+Modal.setAppElement('#root');
+
+const { apiFetch } = wp;
+
+function deleteModel( name = '' ) {
+	if ( ! name.length ) {
+		return;
+	}
+
+	const deleted = apiFetch({
+		path: `/wpe/content-model/${name}`,
+		method: "DELETE",
+		_wpnonce: wpApiSettings.nonce,
+	}).then((res) => {
+		return res;
+	});
+
+	return deleted;
+}
+
+export const ContentModelDropdown = ({model}) => {
+	const { name, slug } = model;
+	const { dispatch } = useContext(ModelsContext);
+	const history = useHistory();
+	const [ dropdownOpen, setDropdownOpen ] = useState(false);
+	const [ modalIsOpen, setModalIsOpen ] = useState(false);
+	const [ editModelModalIsOpen, setEditModelModalIsOpen ] = useState(false);
+
+	const customStyles = {
+		overlay: {
+			backgroundColor: 'rgba(0, 40, 56, 0.7)'
+		},
+		content : {
+			top: '50%',
+			left: '50%',
+			right: 'auto',
+			bottom: 'auto',
+			marginRight: '-50%',
+			transform: 'translate(-50%, -50%)',
+			border: 'none',
+			padding: '40px'
+		}
+	};
+
+	return (
+		<span className="dropdown">
+			<button
+				className="options"
+				aria-label={`Options for ${name} content model`}
+				onClick={ () => {
+					setDropdownOpen(!dropdownOpen);
+				} }
+				onBlur={() => maybeCloseDropdown(setDropdownOpen)}
+			>
+				<Icon type="options" />
+			</button>
+			<div className={`dropdown-content ${dropdownOpen ? '' : 'hidden' }`}>
+				<a
+					className="edit"
+					href="#"
+					onBlur={() => maybeCloseDropdown(setDropdownOpen)}
+					onClick={ (event) => {
+						event.preventDefault();
+						setDropdownOpen(false);
+						setEditModelModalIsOpen(true);
+					} }
+				>
+					Edit
+				</a>
+				<a
+					className="delete"
+					href="#"
+					onBlur={() => maybeCloseDropdown(setDropdownOpen)}
+					onClick={ (event) => {
+						event.preventDefault();
+						setDropdownOpen(false);
+						setModalIsOpen(true);
+					} }
+				>
+					Delete
+				</a>
+			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				contentLabel={`Delete the ${name} content model?`}
+				portalClassName="wpe-content-model-delete-model-modal-container"
+				onRequestClose={() => { setModalIsOpen(false) }}
+				style={customStyles}
+				model={model}
+			>
+				<h2>Delete the {name} Content Model?</h2>
+				<p>This is an irreversible action. You will have to recreate this model if you delete it.</p>
+				<p>This will NOT delete actual data stored in this model. It only deletes the model definition.</p>
+				<p>{`Are you sure you want to delete the ${name} content model?`}</p>
+				<button
+					className="first warning"
+					onClick={ async () => {
+						// @todo capture and show errors.
+						const deleted = await deleteModel(slug);
+						setModalIsOpen(false);
+						history.push( "/wp-admin/admin.php?page=wpe-content-model" );
+						dispatch({type: 'removeModel', slug});
+					}}
+				>
+					Delete
+				</button>
+				<button
+					className="tertiary"
+					onClick={() => {
+						setModalIsOpen(false)
+					}}
+				>
+					Cancel
+				</button>
+			</Modal>
+			<EditModelModal model={model} isOpen={editModelModalIsOpen} setIsOpen={setEditModelModalIsOpen} />
+		</span>
+	);
+}
