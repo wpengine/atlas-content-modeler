7bd6d399ac3b777cf6afc6f3ae71ce50666d7bb4
Nick Cernis <nick@cern.is>
2021-04-08 11:17:06 +0200

    chore: add showSuccess and showError toast helpers
    To simplify displaying error messages without
    having to pass icons and config to `toast()` each
    time.

---
+++ includes/settings/js/src/toasts.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {toast} from "react-toastify";
+import Icon from "./components/icons";
+
+export function showSuccess(message) {
+	toast(
+		() => (
+			<>
+				<Icon type="tick"/>
+				<span>{message}</span>
+			</>
+		),
+		{
+			closeButton: false,
+			toastId: "success",
+			type: "success",
+		}
+	);
+}
+
+export function showError(message) {
+	toast(
+		({closeToast}) => (
+			<>
+				<Icon type="error" size="large" />
+				<span>{message}</span>
+				<button
+					className="close"
+					onClick={closeToast}
+				>
+					<Icon type="close"/>
+				</button>
+			</>
+		),
+		{
+			autoClose: false,
+			closeButton: false,
+			toastId: "error",
+			type: "error",
+		}
+	);
+}
