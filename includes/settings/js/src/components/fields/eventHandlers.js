0fc03b112b29db977c6a2214780d95e584b7248c
Nick Cernis <nick@cern.is>
2021-04-08 11:28:08 +0200

    feat: display a failure toast if reordering fails
    Also:
    - Light refactoring to pull out helper functions.
    - Restore local field order if update fails.

--- includes/settings/js/src/components/fields/eventHandlers.js
+++ includes/settings/js/src/components/fields/eventHandlers.js
@@ -1,20 +1,50 @@
 import {POSITION_GAP} from "../../queries";
+import {toast} from 'react-toastify';
+import {showError} from "../../toasts";
+import React from "react";
 
 const {apiFetch} = wp;
 
 /**
+ * Reorders `list` by moving the item at `startIndex` to `endIndex`.
+ *
+ * @param list
+ * @param startIndex
+ * @param endIndex
+ * @return {Array}
+ * @example
+ * ```
+ * reorder(["a", "b", "c"], 0, 1)); // ["b", "a", "c"]
+ * reorder(["a", "b", "c"], 0, 2)); // ["b", "c", "a"]
+ * ```
+ */
+const reorder = (list, startIndex, endIndex) => {
+	const result = Array.from(list);
+	const [removed] = result.splice(startIndex, 1);
+	result.splice(endIndex, 0, removed);
+
+	return result;
+};
+
+/**
+ * Updates field positions in the WordPress database.
+ *
+ * @return {Promise<void>}
+ */
+const updatePositions = async (model, idsAndPositions) => {
+	await apiFetch({
+		path: `/wpe/content-model-fields/${model}`,
+		method: "PATCH",
+		_wpnonce: wpApiSettings.nonce,
+		data: {fields: idsAndPositions},
+	});
+};
+
+/**
  * Updates the model store and WordPress database when fields are reordered.
  */
-export function onDragEnd(result, fields, model, dispatch) {
+export function onDragEnd(result, fields, model, dispatch, models) {
 	const {destination, source} = result;
-
-	const reorder = (list, startIndex, endIndex) => {
-		const result = Array.from(list);
-		const [removed] = result.splice(startIndex, 1);
-		result.splice(endIndex, 0, removed);
-
-		return result;
-	};
 
 	if (!destination) {
 		return;
@@ -26,6 +56,12 @@ export function onDragEnd(result, fields, model, dispatch) {
 	) {
 		return;
 	}
+
+	// Store original field order to revert if the position update fails.
+	const idsAndOldPositions = fields.reduce((result, id) => {
+		result[id] = {position: models[model]['fields'][id]?.position };
+		return result;
+	}, {});
 
 	const newOrder = reorder(
 		fields,
@@ -40,16 +76,21 @@ export function onDragEnd(result, fields, model, dispatch) {
 		return result;
 	}, {});
 
+	// Optimistically update the client-side model store so the new field order is rendered immediately.
 	dispatch({type: 'reorderFields', positions: idsAndNewPositions, model: model});
 
-	const updatePositions = async () => {
-		await apiFetch({
-			path: `/wpe/content-model-fields/${model}`,
-			method: "PATCH",
-			_wpnonce: wpApiSettings.nonce,
-			data: {fields: idsAndNewPositions},
-		});
-	};
+	updatePositions(model, idsAndNewPositions)
+		.then((res) => {
+			toast.dismiss("error");
+		})
+		.catch((err) => {
+			showError(
+				`Error saving field order: “${
+					err?.message || "Error unknown."
+				}” Close this and try again?`
+			);
 
-	updatePositions().catch(err => console.error(err));
+			// Revert local field order so the state is accurate and the user can retry.
+			dispatch({type: 'reorderFields', positions: idsAndOldPositions, model: model});
+		});
 }
