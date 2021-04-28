9c749a44882f591e8052e190e9d9f1b8101cc4e2
Matthew Wright <matthew.wright@wpengine.com>
2021-04-28 10:54:24 -0500

    updates to get id's to play well with jquery

--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -45,23 +45,20 @@ jQuery(document).ready(function($){
 	$('.wpe-upload-btn').click(function(e) {
 		e.preventDefault();
 		var $btnTarget = $(e.target);
-		console.log($btnTarget.data('field'));
 		var image = wp.media({
 			title: 'Upload Media',
 			// multiple: true if you want to upload multiple files at once
 			multiple: false
 		}).open()
 			.on('select', function(e){
-				console.log('button', $btnTarget);
-				console.log('on select', e);
 				// This will return the selected image from the Media Uploader, the result is an object
 				var uploaded_image = image.state().get('selection').first();
 				// We convert uploaded_image to a JSON object to make accessing it easier
 				// Output to the console uploaded_image
-				console.log(uploaded_image);
-				var image_url = uploaded_image.toJSON().url;
+				var imageData = uploaded_image.toJSON().url;
 				// Let's assign the url value to the input field
-				$('').val(image_url);
+				var $mediaInput = $(`input#${$btnTarget.data('field')}`);
+				$mediaInput.val(imageData);
 			});
 	});
 });
