2adbbfb22a2d6c8d13ee8729cf9f34960095cc5e
Matthew Wright <matthew.wright@wpengine.com>
2021-04-29 08:14:03 -0500

    media uploader working with basics

--- includes/publisher/js/src/components/MediaUploader.jsx
+++ includes/publisher/js/src/components/MediaUploader.jsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 
 export default function MediaUploader({ modelSlug, field }) {
+	const [value, setValue] = useState(field.value);
+	const btnRef = useRef();
+	const inputRef = useRef();
+
+	/**
+	 * Click handler to use wp media uploader
+	 * @param e
+	 */
+	function clickHandler(e) {
+		e.preventDefault();
+
+		wp.media({
+			title: 'Upload Media',
+			// multiple: true if you want to upload multiple files at once
+			multiple: false
+		}).open()
+			.on('select', function(e){
+				// This will return the selected image from the Media Uploader, the result is an object
+				var uploaded_image = image.state().get('selection').first();
+				// We convert uploaded_image to a JSON object to make accessing it easier
+				// Let's assign the url value to the input field
+				inputRef.current = uploaded_image.toJSON().url;
+				console.log(inputRef.current);
+				// {`wpe-content-model[${modelSlug}][${field.slug}]`}
+			});
+	}
+
 	return (
 		<>
 			<label
-				htmlFor={`wpe-content-model-${modelSlug}-${field.slug}`}
+				htmlFor={`wpe-content-model[${modelSlug}][${field.slug}]`}
 			>
 				{field.name}
 			</label>
+
 			<input type="text"
+				   name={`wpe-content-model[${modelSlug}][${field.slug}]`}
+				   id={`wpe-content-model[${modelSlug}][${field.slug}]`}
+				   ref={inputRef}
 				   className="hidden"
-				   name={`wpe-content-model-${modelSlug}-${field.slug}`}
-				   id={`wpe-content-model-${modelSlug}-${field.slug}`}
-				   defaultValue={field.value} />
+				   defaultValue={value} />
+
 			<div>
 				<input type="button"
-					   className="button button-primary button-large wpe-upload-btn"
-					   data-field={`wpe-content-model-${modelSlug}-${field.slug}`}
-					   name={`upload_btn_${field.slug}`}
-					   id={`upload_btn_${field.slug}`}
-					   defaultValue="Upload Media" />
-				{field.value && (
-					<img src={field.value} alt={field.name} />
-				)}
+				   	ref={btnRef}
+					defaultValue="Upload Media"
+					onClick={(e) => clickHandler(e)}
+				/>
 			</div>
+
+			{value && (
+				<div>
+					<img src={value} alt={field.name} />
+				</div>
+			)}
 		</>
 	);
 }
--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -45,25 +45,3 @@ window.addEventListener("DOMContentLoaded", (event) => {
 		tinymce.execCommand("mceAddEditor", false, field)
 	);
 });
-
-jQuery(document).ready(function($){
-	$('.wpe-upload-btn').click(function(e) {
-		e.preventDefault();
-		var $btnTarget = $(e.target);
-		var image = wp.media({
-			title: 'Upload Media',
-			// multiple: true if you want to upload multiple files at once
-			multiple: false
-		}).open()
-			.on('select', function(e){
-				// This will return the selected image from the Media Uploader, the result is an object
-				var uploaded_image = image.state().get('selection').first();
-				// We convert uploaded_image to a JSON object to make accessing it easier
-				// Let's assign the url value to the input field
-				var input = document.getElementById($btnTarget.data('field'));
-				input.value = uploaded_image.toJSON().url;
-
-				console.log(input.value);
-			});
-	});
-});
