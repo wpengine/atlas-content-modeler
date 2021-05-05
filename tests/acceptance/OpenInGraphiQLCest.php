dc972db2516918b24fd097099eab97071d40fdf4
Nick Cernis <nick@cern.is>
2021-05-05 19:21:24 +0200

    test: check the “Open in GraphiQL” option works

---
+++ tests/acceptance/OpenInGraphiQLCest.php
@@ -0,0 +1,28 @@
+<?php
+
+class OpenInGraphiQLCest
+{
+    /**
+     * Ensure the “Open in GraphiQL” model option opens GraphiQL with a pre-filled query.
+     */
+    public function i_can_open_my_model_in_graphiql(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+        $I->haveContentModel('Dog', 'Dogs');
+        $I->amOnWPEngineEditContentModelPage('dogs');
+        $I->wait(1);
+
+        $I->click('Text', '.field-buttons');
+		$I->wait(1);
+		$I->fillField(['name' => 'name'], 'Wags per minute');
+        $I->click('.open-field button.primary');
+        $I->wait(1);
+
+		$I->click('.heading .options'); // Model options button.
+		$I->click('.show-in-graphiql');
+		$I->switchToNextTab(); // GraphiQL opens in a new tab.
+		$I->wait(1);
+
+		$I->see('wagsPerMinute', '#graphiql');
+	}
+}
