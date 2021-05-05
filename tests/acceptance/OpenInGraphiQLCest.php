8b2717a44013fb1df13f7668a336014e2f706560
Nick Cernis <nick@cern.is>
2021-05-05 19:34:34 +0200

    chore: spaces to tabs

--- tests/acceptance/OpenInGraphiQLCest.php
+++ tests/acceptance/OpenInGraphiQLCest.php
@@ -2,21 +2,21 @@
 
 class OpenInGraphiQLCest
 {
-    /**
-     * Ensure the “Open in GraphiQL” model option opens GraphiQL with a pre-filled query.
-     */
-    public function i_can_open_my_model_in_graphiql(AcceptanceTester $I)
-    {
-        $I->loginAsAdmin();
-        $I->haveContentModel('Dog', 'Dogs');
-        $I->amOnWPEngineEditContentModelPage('dogs');
-        $I->wait(1);
+	/**
+	 * Ensure the “Open in GraphiQL” model option opens GraphiQL with a pre-filled query.
+	 */
+	public function i_can_open_my_model_in_graphiql(AcceptanceTester $I)
+	{
+		$I->loginAsAdmin();
+		$I->haveContentModel('Dog', 'Dogs');
+		$I->amOnWPEngineEditContentModelPage('dogs');
+		$I->wait(1);
 
-        $I->click('Text', '.field-buttons');
+		$I->click('Text', '.field-buttons');
 		$I->wait(1);
 		$I->fillField(['name' => 'name'], 'Wags per minute');
-        $I->click('.open-field button.primary');
-        $I->wait(1);
+		$I->click('.open-field button.primary');
+		$I->wait(1);
 
 		$I->click('.heading .options'); // Model options button.
 		$I->click('.show-in-graphiql');
