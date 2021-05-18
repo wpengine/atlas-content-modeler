35d445d8f302bac7fb113e978897ecc90bddca7e
Nick Cernis <nick@cern.is>
2021-05-18 11:59:05 +0200

    BH-964: add server-side field sanitization (#105)
    * feat: add field sanitization functions
    
    * test: verify field sanitization functions
    
    * test: acceptance test for text field sanitization

---
+++ tests/acceptance/PublishModelSanitizationCest.php
@@ -0,0 +1,37 @@
+<?php
+use Codeception\Util\Locator;
+class PublishModelSanitizationCest
+{
+	public function i_can_publish_a_model_and_fields_are_sanitized(AcceptanceTester $i)
+	{
+		$i->maximizeWindow();
+
+		// Create a model.
+		$i->loginAsAdmin();
+		$i->haveContentModel('goose', 'geese', 'Geese go honk');
+		$i->wait(1);
+
+		// Add a text field.
+		$i->click('Text', '.field-buttons');
+		$i->fillField(['name' => 'name'], 'Color');
+		$i->click('.open-field button.primary');
+		$i->wait(1);
+
+		// Create a new goose.
+		$i->amOnPage('/wp-admin/edit.php?post_type=geese');
+		$i->click('Add New', '.wrap');
+		$i->wait(1);
+
+		// Fill the color field, including HTML tags that should be stripped.
+		$i->fillField(['name' => 'wpe-content-model[geese][color]'], '<em>Gray</em>');
+
+		$i->click('Publish', '#publishing-action');
+		$i->wait(2);
+
+		$i->see('Post published.');
+
+		// HTML tags should be stripped in text fields if sanitization is working, with tag content preserved.
+		$i->dontSeeInField('wpe-content-model[geese][color]', '<em>Gray</em>');
+		$i->seeInField('wpe-content-model[geese][color]', 'Gray');
+	}
+}
--- tests/integration/publisher/test-field-functions.php
+++ tests/integration/publisher/test-field-functions.php
@@ -8,6 +8,8 @@
 use function WPE\ContentModel\order_fields;
 use function WPE\ContentModel\get_top_level_fields;
 use function WPE\ContentModel\get_entry_title_field;
+use function WPE\ContentModel\sanitize_field;
+use function WPE\ContentModel\get_field_type_from_slug;
 
 /**
  * Class FieldFunctionTestCases
@@ -73,5 +75,86 @@ class FieldFunctionTestCases extends WP_UnitTestCase {
 			$expected,
 			get_entry_title_field( $fields )
 		);
+	}
+
+	public function test_get_field_type_from_slug(): void {
+		$slug_to_find = 'findme';
+
+		$fields = [
+			[ 'type' => 'text', 'slug' => 'test', ],
+			[ 'type' => 'media', 'slug' => $slug_to_find ],
+		];
+
+		$expected = 'media';
+
+		$this->assertSame(
+			$expected,
+			get_field_type_from_slug( $slug_to_find, $fields )
+		);
+	}
+
+	public function test_get_field_type_from_slug_missing_type(): void {
+		$slug_to_find = 'findme';
+
+		$fields = [
+			[ 'slug' => 'test', ],
+			[ 'slug' => $slug_to_find ],
+		];
+
+		$expected = 'unknown';
+
+		$this->assertSame(
+			$expected,
+			get_field_type_from_slug( $slug_to_find, $fields )
+		);
+	}
+
+	public function test_get_field_type_from_slug_no_slug_matches(): void {
+		$slug_to_find = 'findme';
+
+		$fields = [
+			[ 'slug' => 'test', ],
+			[ 'slug' => 'test2' ],
+			[ 'slug' => 'test3' ],
+		];
+
+		$expected = 'unknown';
+
+		$this->assertSame(
+			$expected,
+			get_field_type_from_slug( $slug_to_find, $fields )
+		);
+	}
+
+	public function test_sanitize_field(): void {
+		$test_cases = [
+			[ 'text', '<p>Test</p>', 'Test' ],
+			[ 'richtext', '<em>Test</em><script></script>', '<em>Test</em>' ],
+			[ 'number', '123', '123' ],
+			[ 'number', '-123', '-123' ],
+			[ 'number', '1.23', '1.23' ],
+			[ 'number', '123abc', '123' ],
+			[ 'number', '1.23abc', '1.23' ],
+			[ 'number', '1,000.00', '1000.00' ],
+			[ 'number', '1.000,00', '1.00000' ],
+			[ 'date', '2021-12-31', '2021-12-31' ],
+			[ 'date', '2021-31-12', '' ],
+			[ 'date', '12-31-2021', '' ],
+			[ 'date', 'not-a-date', '' ],
+			[ 'media', '123', '123' ],
+			[ 'media', 'not-a-number', '' ],
+			[ 'boolean', 'on', 'on' ],
+			[ 'boolean', 'other', 'off' ],
+			[ 'unknown-type', 'unaffected', 'unaffected' ],
+		];
+
+		foreach ( $test_cases as $test ) {
+			[ $type, $input, $expected ] = $test;
+
+			$this->assertSame(
+				$expected,
+				sanitize_field( $type, $input )
+			);
+		}
 	}
 }
