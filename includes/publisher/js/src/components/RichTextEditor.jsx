2c373c39c1739135eef54fc4a67f742c6d54cd82
Matthew Wright <matthew.wright@wpengine.com>
2021-05-27 09:07:50 -0500

    Update RichTextEditor.jsx

--- includes/publisher/js/src/components/RichTextEditor.jsx
+++ includes/publisher/js/src/components/RichTextEditor.jsx
@@ -9,21 +9,19 @@ export default function RichTextEditor({
 	validate,
 	defaultError,
 }) {
+	const fieldId = `wpe-content-model-${modelSlug}-${field.slug}`;
+
 	useEffect(() => {
-		console.log(field);
-		wp.oldEditor.initialize(
-			`wpe-content-model-${modelSlug}-${field.slug}`,
-			{
-				...wp.oldEditor.getDefaultSettings(),
-				tinymce: {
-					height: "300",
-					toolbar1:
-						"undo,redo,formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media",
-				},
-				mediaButtons: true,
-				quicktags: false,
-			}
-		);
+		wp.oldEditor.initialize(fieldId, {
+			...wp.oldEditor.getDefaultSettings(),
+			tinymce: {
+				height: "300",
+				toolbar1:
+					"undo,redo,formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media",
+			},
+			mediaButtons: true,
+			quicktags: false,
+		});
 	}, []);
 
 	return (
@@ -34,7 +32,7 @@ export default function RichTextEditor({
 			<br />
 			<textarea
 				name={`wpe-content-model[${modelSlug}][${field.slug}]`}
-				id={`wpe-content-model-${modelSlug}-${field.slug}`}
+				id={fieldId}
 				defaultValue={field.value}
 				required={field.required}
 			/>
