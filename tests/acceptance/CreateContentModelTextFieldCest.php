81c129436056a323a389744bbed073472b356db5
Nick Cernis <nick@cern.is>
2021-06-10 15:42:06 +0200

    BH-1058: Change “Text Length” to “Input Type” in the Text field (#136)
    * fix: change “Text Length” to “Input Type”
    
    Text length is now determined by the min and max
    options in the Text field's Advanced Settings
    modal.
    
    Input Type allows devs to specify an input field
    for single line text, or a textarea for multiline
    text.
    
    * tests: replace textLength with inputType
    
    * fix: use textarea if inputType is 'multi'
    
    So that publishers see the input type specified
    by the developer.
    
    * test: check multiline Text fields make a textarea

--- tests/acceptance/CreateContentModelTextFieldCest.php
+++ tests/acceptance/CreateContentModelTextFieldCest.php
@@ -22,4 +22,34 @@ class CreateContentModelTextFieldCest
         $I->see('Text', '.field-list div.type');
         $I->see('Color', '.field-list div.widest');
     }
+
+	public function i_can_create_a_content_model_text_field_as_a_textarea(AcceptanceTester $I)
+	{
+		$I->loginAsAdmin();
+		$I->haveContentModel('Candy', 'Candies');
+		$I->wait(1);
+
+		$I->click('Text', '.field-buttons');
+		$I->wait(1);
+		$I->fillField(['name' => 'name'], 'Color');
+		$I->seeInField('#slug','color');
+
+		// Set the Input Type to “multiple lines” instead of “Single line”.
+		$I->click('#multi');
+
+		// Save the field.
+		$I->click('.open-field button.primary');
+		$I->wait(1);
+
+		// Create a new Candies entry.
+		$I->amOnPage('/wp-admin/edit.php?post_type=candy');
+		$I->click('Add New', '.wrap');
+		$I->wait(1);
+
+		// Check that the “Color” field uses a textarea instead of an input field.
+		$I->seeElement(
+			'textarea',
+			[ 'name' => 'atlas-content-modeler[candy][color]' ]
+		);
+	}
 }
