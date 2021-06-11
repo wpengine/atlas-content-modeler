d711e0150ceeb7d6e8184865a6cd2f91af379ad4
Nick Cernis <nick@cern.is>
2021-06-11 16:21:12 +0200

    feature: add Unsaved Changes modal and logic

--- includes/settings/js/src/components/EditContentModel.jsx
+++ includes/settings/js/src/components/EditContentModel.jsx
@@ -1,32 +1,87 @@
-import React, { useContext, useEffect, useState, useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { useLocationSearch } from "../utils";
 import { onDragEnd } from "./fields/eventHandlers";
 import Field from "./fields/Field";
 import { ModelsContext } from "../ModelsContext";
 import { ContentModelDropdown } from "./ContentModelDropdown";
+import Modal from "react-modal";
 
-import { getFieldOrder, getPositionAfter, sanitizeFields } from "../queries";
+import {
+	getFieldOrder,
+	getPositionAfter,
+	sanitizeFields,
+	getOpenField,
+} from "../queries";
 import FieldButtons from "./FieldButtons";
+
+Modal.setAppElement("#root");
 
 export default function EditContentModel() {
 	const [infoTag, setInfoTag] = useState(null);
 	const { models, dispatch } = useContext(ModelsContext);
+	const [unsavedChangesModal, updateUnsavedChangesModal] = useState({
+		open: false,
+		field: {},
+	});
 	const query = useLocationSearch();
 	const id = query.get("id");
 	const model = models?.hasOwnProperty(id) ? models[id] : {};
 	const fields = model?.fields ? sanitizeFields(model.fields) : {};
 	const fieldCount = Object.keys(fields).length;
 	const fieldOrder = getFieldOrder(fields);
+	const history = useHistory();
+
+	const customStyles = {
+		overlay: {
+			backgroundColor: "rgba(0, 40, 56, 0.7)",
+		},
+		content: {
+			top: "50%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)",
+			border: "none",
+			padding: "40px",
+		},
+	};
+
+	const promptToSaveChanges = () => {
+		const openField = getOpenField(fields);
+		if (Object.keys(openField).length > 0) {
+			updateUnsavedChangesModal({ open: true, field: openField });
+			return true;
+		}
+		return false;
+	};
+
+	const closeUnsavedChangesModal = () => {
+		updateUnsavedChangesModal({
+			open: false,
+			field: {},
+		});
+	};
+
+	console.log(unsavedChangesModal);
 
 	return (
 		<div className="app-card">
 			<section className="heading">
 				<h2 className="pr-1 pr-sm-0">
-					<Link to="/wp-admin/admin.php?page=atlas-content-modeler">
+					<a
+						href="#"
+						onClick={(event) => {
+							event.preventDefault();
+							if (!promptToSaveChanges()) {
+								history.push(atlasContentModeler.appPath);
+							}
+						}}
+					>
 						Content Models
-					</Link>{" "}
+					</a>{" "}
 					/ {model?.plural}
 				</h2>
 				<ContentModelDropdown model={model} />
@@ -76,6 +131,9 @@ export default function EditContentModel() {
 														editing={editing}
 														data={fields[id]}
 														setInfoTag={setInfoTag}
+														promptToSaveChanges={
+															promptToSaveChanges
+														}
 														position={position}
 														positionAfter={getPositionAfter(
 															id,
@@ -90,6 +148,46 @@ export default function EditContentModel() {
 								</Droppable>
 							</DragDropContext>
 						</ul>
+						<Modal
+							isOpen={unsavedChangesModal.open}
+							contentLabel={`Unsaved Changes`}
+							portalClassName="atlas-content-modeler-unsaved-changes-modal-container"
+							onRequestClose={() => {
+								closeUnsavedChangesModal();
+							}}
+							style={customStyles}
+							model={model}
+						>
+							<h2>Unsaved Changes</h2>
+							<p>
+								Would you like to discard your{" "}
+								{unsavedChangesModal.field?.name}{" "}
+								{unsavedChangesModal.field?.type} field updates?
+							</p>
+							<button
+								className="first primary"
+								onClick={() => closeUnsavedChangesModal()}
+							>
+								Continue Editing
+							</button>
+							<button
+								className="tertiary"
+								onClick={() => {
+									const action = unsavedChangesModal?.field
+										?.editing
+										? "closeField"
+										: "removeField";
+									dispatch({
+										type: action,
+										model: id,
+										id: unsavedChangesModal?.field?.id,
+									});
+									closeUnsavedChangesModal();
+								}}
+							>
+								Discard Changes
+							</button>
+						</Modal>
 					</>
 				) : (
 					<>
