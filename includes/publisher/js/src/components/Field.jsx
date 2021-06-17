328bc1f96b600f556c8c2d0b4d94d361d0ddb9a0
Anthony Burchell <anthony.burchell@wpengine.com>
2021-06-16 23:16:27 -0500

    validate value exists before running an array function on it

--- includes/publisher/js/src/components/Field.jsx
+++ includes/publisher/js/src/components/Field.jsx
@@ -199,7 +199,7 @@ function fieldMarkup(field, modelSlug, errors, validate) {
 										name={`atlas-content-modeler[${modelSlug}][${field.slug}][${index}][${item.name}]`}
 										id={`atlas-content-modeler[${modelSlug}][${field.slug}][${index}][${item.name}]`}
 										placeholder="Option Name"
-										defaultChecked={field.value.some(
+										defaultChecked={field.value && field.value.some(
 											(name) => name == item.name
 										)}
 									/>
