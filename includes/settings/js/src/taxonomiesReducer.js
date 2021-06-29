8c0458a20ac2f1df2cecd5d9b9dc1376f2aedf4b
Nick Cernis <nick@cern.is>
2021-06-29 21:12:06 +0200

    chore: create addTaxonomy reducer action
    So that the app immediately reflects taxonomy
    state after successful taxonomy creation.

--- includes/settings/js/src/taxonomiesReducer.js
+++ includes/settings/js/src/taxonomiesReducer.js
@@ -4,6 +4,11 @@
 
 export function taxonomiesReducer(state, action) {
 	switch (action.type) {
+		case "addTaxonomy":
+			return {
+				...state,
+				[action.data.slug]: action.data,
+			};
 		default:
 			throw new Error(`${action.type} not found`);
 	}
