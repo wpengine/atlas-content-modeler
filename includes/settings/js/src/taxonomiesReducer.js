246e5186b97b540e2feccce528f403e916798c34
Nick Cernis <nick@cern.is>
2021-06-29 11:12:06 +0200

    chore: add taxonomies reducer
    The main models reducer is already fairly complex
    but well scoped to just model and field data.
    Keeping the taxonomies reducer separate should
    help us to manage and maintain model and taxonomy
    state independently.

---
+++ includes/settings/js/src/taxonomiesReducer.js
@@ -0,0 +1,10 @@
+/**
+ * Actions to update global taxonomy state.
+ */
+
+export function taxonomiesReducer(state, action) {
+	switch (action.type) {
+		default:
+			throw new Error(`${action.type} not found`);
+	}
+}
