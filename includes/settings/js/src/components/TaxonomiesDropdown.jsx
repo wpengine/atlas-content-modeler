d33113b4c873a5fccefad6324b34f9f484f2c958
Nick Cernis <nick@cern.is>
2021-07-01 20:28:29 +0200

    BH-1078: Add Taxonomy List (#187)
    - Implements the taxonomy table in the taxonomies view.
    - Adds the taxonomy options button and drop down.
    - Adds “Edit” and “Delete” options to the drop down.
    
    To follow:
    
    - Edit and Delete taxonomy implementations.

---
+++ includes/settings/js/src/components/TaxonomiesDropdown.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import Icon from "../../../../components/icons";
+import Modal from "react-modal";
+import { maybeCloseDropdown } from "../utils";
+import { sprintf, __ } from "@wordpress/i18n";
+
+export const TaxonomiesDropdown = ({ taxonomy }) => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const timer = useRef(null);
+
+	const customStyles = {
+		overlay: {
+			backgroundColor: "rgba(0, 40, 56, 0.7)",
+		},
+		content: {
+			top: "50%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)",
+			border: "none",
+			padding: "40px",
+		},
+	};
+
+	const handleKeyPress = useCallback(
+		(e) => {
+			if (e.key === "Escape") {
+				setDropdownOpen(false);
+			}
+		},
+		[setDropdownOpen]
+	);
+
+	useEffect(() => {
+		if (dropdownOpen) {
+			document.addEventListener("keydown", handleKeyPress);
+		} else {
+			document.removeEventListener("keydown", handleKeyPress);
+		}
+
+		return () => document.removeEventListener("keydown", handleKeyPress);
+	}, [dropdownOpen, handleKeyPress]);
+
+	useEffect(() => {
+		return () => clearTimeout(timer.current);
+	}, [timer]);
+
+	return (
+		<span className="dropdown">
+			<button
+				className="options py-sm-0 py-2"
+				onBlur={() => maybeCloseDropdown(setDropdownOpen, timer)}
+				onClick={() => setDropdownOpen(!dropdownOpen)}
+				aria-label={sprintf(
+					__("Options for the %s taxonomy.", "atlas-content-modeler"),
+					taxonomy.plural
+				)}
+			>
+				<Icon type="options" />
+			</button>
+			<div className={`dropdown-content ${dropdownOpen ? "" : "hidden"}`}>
+				<a
+					href="#"
+					aria-label={sprintf(
+						__("Edit the %s taxonomy.", "atlas-content-modeler"),
+						taxonomy.plural
+					)}
+					onBlur={() => maybeCloseDropdown(setDropdownOpen, timer)}
+					onClick={(event) => {
+						event.preventDefault();
+						// TODO: implement editing here.
+						alert("TODO: Implement editing here.");
+						setDropdownOpen(false);
+					}}
+				>
+					{__("Edit", "atlas-content-modeler")}
+				</a>
+				<a
+					className="delete"
+					href="#"
+					aria-label={sprintf(
+						__("Delete the %s taxonomy.", "atlas-content-modeler"),
+						taxonomy.plural
+					)}
+					onBlur={() => maybeCloseDropdown(setDropdownOpen, timer)}
+					onClick={(event) => {
+						event.preventDefault();
+						setDropdownOpen(false);
+						setModalIsOpen(true);
+					}}
+				>
+					{__("Delete", "atlas-content-modeler")}
+				</a>
+			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				contentLabel={sprintf(
+					__("Delete the %s taxonomy?", "atlas-content-modeler"),
+					taxonomy.plural
+				)}
+				portalClassName="atlas-content-modeler-delete-field-modal-container"
+				onRequestClose={() => {
+					setModalIsOpen(false);
+				}}
+				// taxonomy={taxonomy}
+				style={customStyles}
+			>
+				<h2>
+					{sprintf(
+						__("Delete the %s taxonomy?", "atlas-content-modeler"),
+						taxonomy.plural
+					)}
+				</h2>
+				<p>
+					{__(
+						"This will delete the taxonomy and related data.",
+						"atlas-content-modeler"
+					)}
+				</p>
+				<p>
+					{sprintf(
+						__(
+							"Are you sure you want to delete the %s taxonomy? ",
+							"atlas-content-modeler"
+						),
+						taxonomy.plural
+					)}
+				</p>
+				<button
+					type="submit"
+					form={taxonomy.slug}
+					className="first warning"
+					onClick={() => {
+						// TODO: implement deletion here. See FieldOptionsDropdown.jsx.
+						alert("TODO: implement deletion here.");
+						setModalIsOpen(false);
+					}}
+				>
+					{__("Delete", "atlas-content-modeler")}
+				</button>
+				<button
+					className="tertiary"
+					onClick={() => {
+						setModalIsOpen(false);
+					}}
+				>
+					{__("Cancel", "atlas-content-modeler")}
+				</button>
+			</Modal>
+		</span>
+	);
+};
