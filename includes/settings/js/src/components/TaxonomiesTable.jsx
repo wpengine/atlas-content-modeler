90ca814c889a36761a2916b324b6c9a5dc4383e2
Nick Cernis <nick@cern.is>
2021-07-01 16:01:16 +0200

    revert: remove bulk actions for now
    After discussion with @mindctrl we plan to defer
    bulk actions to implement later.

--- includes/settings/js/src/components/TaxonomiesTable.jsx
+++ includes/settings/js/src/components/TaxonomiesTable.jsx
@@ -5,26 +5,9 @@ import { TaxonomiesDropdown } from "./TaxonomiesDropdown";
 const TaxonomiesTable = ({ taxonomies = {} }) => {
 	return (
 		<>
-			<div className="bulk-actions">
-				<select name="bulk-action">
-					<option value="-1">Bulk action</option>
-					<option value="delete">Delete</option>
-				</select>
-				<button className="tertiary">Apply</button>
-			</div>
 			<table className="table table-striped">
 				<thead>
 					<tr>
-						<th className="checkbox">
-							<input
-								type="checkbox"
-								className="check-all"
-								aria-label={__(
-									"Toggle selection of all taxonomies to apply a bulk action",
-									"atlas-content-modeler"
-								)}
-							/>
-						</th>
 						<th>{__("Name", "atlas-content-modeler")}</th>
 						<th>{__("Slug", "atlas-content-modeler")}</th>
 						<th>{__("Models", "atlas-content-modeler")}</th>
@@ -37,19 +20,6 @@ const TaxonomiesTable = ({ taxonomies = {} }) => {
 					{Object.values(taxonomies).map((taxonomy) => {
 						return (
 							<tr key={taxonomy?.slug}>
-								<td className="checkbox">
-									<input
-										type="checkbox"
-										className="check-all"
-										aria-label={sprintf(
-											__(
-												"Toggle selection of the %s taxonomy.",
-												"atlas-content-modeler"
-											),
-											taxonomy.plural
-										)}
-									/>
-								</td>
 								<td>{taxonomy?.plural}</td>
 								<td>{taxonomy?.slug}</td>
 								<td>{taxonomy?.types?.join(", ")}</td>
