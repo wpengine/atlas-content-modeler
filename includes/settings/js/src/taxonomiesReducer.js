e6283af129d05e6cfb9131083c0f1bacd03f1c36
Andrew Matthews <andrew.matthews@wpengine.com>
2021-07-08 09:27:18 -0400

    [BH-1077] Delete taxonomies (#191)
    * feat: add delete taxonomy functionality
    
    * refactor: simplify parameter retrieval
    
    * refactor: improve error response
    
    * test: add tests for delete REST routes
    
    * refactor: use taxonomy plural name in error message
    
    * chore: convert commented code to TODO
    
    * test: add acceptance test for taxonomy deletion
    
    * chore: ensure types is an array in haveTaxonomy helper
    
    * chore: amend spacing to trigger CircleCI
    
    Circle had downtime yesterday and doesn't seem
    to have found this new branch.
    
    This is a spacing change purely to try to trigger
    Circle workflows.
    
    * style: convert spaces to tabs
    
    Co-authored-by: Nick Cernis <nick@cern.is>

--- includes/settings/js/src/taxonomiesReducer.js
+++ includes/settings/js/src/taxonomiesReducer.js
@@ -9,6 +9,9 @@ export function taxonomiesReducer(state, action) {
 				...state,
 				[action.data.slug]: action.data,
 			};
+		case "removeTaxonomy":
+			const { [action.slug]: deleted, ...otherTaxonomies } = state;
+			return otherTaxonomies;
 		default:
 			throw new Error(`${action.type} not found`);
 	}
