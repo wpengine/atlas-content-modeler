f6dd0c2dbd843e490a336c95e6ec9a63e8d856f6
Nick Cernis <nick@cern.is>
2021-07-08 13:07:06 +0200

    test: only posts with post types are filtered
    If WP triggers the post_title filter but cannot
    find post information, the post title filter
    should just yield the passed post title and
    not trigger a PHP warning.
    
    This is a defensive test because the
    post_title filter is triggered on the WP
    login screen, even though no posts
    appear there.

---
+++ tests/integration/publisher/test-title-filter.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tests Title Filtering.
+ *
+ * @package AtlasContentModeler
+ */
+
+/**
+ * Class TestTitleFilter
+ */
+class TestTitleFilter extends WP_UnitTestCase {
+
+	private $form_editing_experience;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->form_editing_experience = new \WPE\AtlasContentModeler\FormEditingExperience();
+	}
+
+	/**
+	 * Ensures the title filter only applies if posts have a known post type.
+	 *
+	 * @covers \WPE\AtlasContentModeler\FormEditingExperience()->filter_post_titles();
+	 */
+	public function test_title_filter_returns_unaltered_title_if_post_has_no_post_type(): void {
+		$original_title = "Original title";
+
+		$filtered_title = $this->form_editing_experience->filter_post_titles(
+			$original_title,
+			123456789 // To test that no filtering takes place if a post cannot be found.
+		);
+
+		$this->assertSame( $original_title, $filtered_title );
+	}
+}
