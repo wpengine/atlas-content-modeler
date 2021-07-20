18aa1d4b6b7338f133bcb22bb1717d24c689b0ac
Andrew Matthews <andrew.matthews@wpengine.com>
2021-07-20 17:15:21 -0400

    feat: update the admin sidebar when taxonomies are changed

--- includes/settings/js/src/components/Taxonomies.jsx
+++ includes/settings/js/src/components/Taxonomies.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { __, sprintf } from "@wordpress/i18n";
 import { useLocationSearch } from "../utils";
@@ -12,6 +12,79 @@ export default function Taxonomies() {
 	const query = useLocationSearch();
 	const editing = query.get("editing");
 	const editingTaxonomy = editing ? taxonomies[editing] : null;
+
+	const prevTaxonomiesRef = useRef();
+	useEffect(() => {
+		prevTaxonomiesRef.current = taxonomies;
+	});
+	const prevTaxonomies = prevTaxonomiesRef.current;
+
+	useEffect(() => {
+		if (!taxonomies || !prevTaxonomies) {
+			// If no prevTaxonomies, then nothing has been changed.
+			return;
+		}
+
+		const getTaxonomyEditHref = (slug, type) =>
+			`edit-tags.php?taxonomy=${slug}&post_type=${type}`;
+
+		// Remove taxonomies that no longer exists or are not associated with a specific model.
+		Object.values(prevTaxonomies).forEach(({ slug, types }) => {
+			types.forEach((type) => {
+				if (
+					!taxonomies.hasOwnProperty(slug) ||
+					!taxonomies[slug].types.includes(type)
+				) {
+					const href = getTaxonomyEditHref(slug, type);
+					const taxLink = document.querySelector(
+						`#adminmenu a[href="${href}"]`
+					);
+
+					if (!taxLink) {
+						// This model for this "type" must have been deleted.
+						return;
+					}
+
+					// Just hide the list item so we can preserve order if this type is re-assigned.
+					taxLink.parentElement.style.display = "none";
+				}
+			});
+		});
+
+		// Add or update all current taxonomies.
+		Object.values(taxonomies).forEach(({ slug, plural, types }) => {
+			types.forEach((type) => {
+				const href = getTaxonomyEditHref(slug, type);
+				const taxLink = document.querySelector(
+					`#adminmenu a[href="${href}"`
+				);
+
+				// Get or create the list item.
+				let listItem = taxLink
+					? taxLink.parentElement
+					: document.createElement("li");
+
+				// Give the list item an updated edit link.
+				listItem.innerHTML = `<a href="${href}">${plural}</a>`;
+				// Always show taxonomies that are enabled in case we hid them on removal.
+				listItem.style.display = "list-item";
+
+				// If the taxonomy link didn't exist already, we should insert it.
+				if (!taxLink) {
+					const postTypeSubMenu = document.querySelector(
+						`#menu-posts-${type} .wp-submenu`
+					);
+
+					if (!postTypeSubMenu) {
+						// The model for this "type" must have been deleted.
+						return;
+					}
+
+					postTypeSubMenu.appendChild(listItem);
+				}
+			});
+		});
+	}, [taxonomies]);
 
 	const cancelEditing = () => {
 		history.push(atlasContentModeler.appPath + "&view=taxonomies");
