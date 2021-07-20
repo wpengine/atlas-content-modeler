8c47cda08480b1c431670a6e093579f79a6a2a46
Nick Cernis <nick@cern.is>
2021-07-20 17:16:45 +0200

    chore: remove unused import

--- includes/settings/js/src/components/Taxonomies.jsx
+++ includes/settings/js/src/components/Taxonomies.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { __, sprintf } from "@wordpress/i18n";
 import { useLocationSearch } from "../utils";
