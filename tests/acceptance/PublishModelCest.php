80b5629e2df075abfd0260acb3dec4a0211c87b8
Matthew Wright <matthew.wright@wpengine.com>
2021-07-27 10:03:49 -0500

    first test to "test"

--- tests/acceptance/PublishModelCest.php
+++ tests/acceptance/PublishModelCest.php
@@ -12,7 +12,7 @@ class PublishModelCest
 		$i->wait(1);
 	}
 
-	public function error_is_triggered_for_min_value(\AcceptanceTester $i)
+	public function error_is_triggered_for_required_value(\AcceptanceTester $i)
 	{
 		$i->click('Number', '.field-buttons');
 		$i->fillField(['name' => 'name'], 'Integer');
@@ -22,7 +22,7 @@ class PublishModelCest
 		$i->click(Locator::lastElement('.add-item'));
 		$i->click('Number', '.field-buttons');
 		$i->fillField(['name' => 'name'], 'Decimal');
-		// $i->checkOption('');
+		$i->click('input#decimal');
 		$i->click('.open-field button.primary');
 		$i->wait(1);
 
@@ -31,48 +31,20 @@ class PublishModelCest
 		$i->click('Add New', '.wrap');
 		$i->wait(1);
 
-		$i->fillField(['name' => 'atlas-content-modeler[goose][color]'], 'Gray');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][age]'], '100');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][dateOfBirth]'], '01/01/2021');
-		$i->checkOption('atlas-content-modeler[goose][fliesSouthForWinter]');
+		$i->fillField(['name' => 'atlas-content-modeler[goose][integer]'], '');
+		$i->fillField(['name' => 'atlas-content-modeler[goose][decimal]'], '');
 
-		// Rich text fields rendered as TinyMCE live in an iframe.
-		$i->switchToIFrame('#field-description iframe');
-		$i->fillField('#tinymce', 'I am a goose');
-		$i->switchToIFrame(); // switch back to main window
-
-		// Fill the second TinyMCE field.
-		$i->switchToIFrame('#field-anotherRichTextField iframe');
-		$i->fillField('#tinymce', 'I am another rich text field');
-		$i->switchToIFrame(); // switch back to main window
 		$i->scrollTo('#submitdiv');
-
 
 		$i->click('Publish', '#publishing-action');
 		$i->wait(2);
 
-		$i->see('Post published.');
+		$i->see('This field is required.');
 		$i->wait(1);
 		$i->see('Edit goose'); // Page title should change from “Add goose” when published.
 
-		$i->seeInField('atlas-content-modeler[goose][color]', 'Gray');
-		$i->seeInField('atlas-content-modeler[goose][age]', '100');
-		$i->seeInField('atlas-content-modeler[goose][dateOfBirth]', '2021-01-01');
-		$i->seeCheckboxIsChecked('atlas-content-modeler[goose][fliesSouthForWinter]');
-		$i->switchToIFrame('#field-description iframe');
-		$i->see('I am a goose'); // Sees the text in the TinyMCE iframe body.
-		$i->switchToIFrame();
-
-		// Show <textarea> elements hidden by TinyMCE so we can see them to check their values directly.
-		$i->executeJS("
-			var field = document.getElementsByName('atlas-content-modeler[goose][description]');
-			field[0].removeAttribute('style');
-			var fieldTwo = document.getElementsByName('atlas-content-modeler[goose][anotherRichTextField]');
-			fieldTwo[0].removeAttribute('style');
-		");
-
-		$i->seeInField('atlas-content-modeler[goose][description]', '<p>I am a goose</p>');
-		$i->seeInField('atlas-content-modeler[goose][anotherRichTextField]', '<p>I am another rich text field</p>');
+		$i->seeInField('atlas-content-modeler[goose][integer]', '');
+		$i->seeInField('atlas-content-modeler[goose][decimal]', '');
 	}
 
 	public function i_can_publish_a_model_entry(AcceptanceTester $i)
