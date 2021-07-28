d6c8a7171c118c3ccf11d78ceeeefb40aa9b936a
Matthew Wright <matthew.wright@wpengine.com>
2021-07-28 09:33:22 -0500

    make update

--- tests/acceptance/PublishModelCest.php
+++ tests/acceptance/PublishModelCest.php
@@ -8,7 +8,7 @@ class PublishModelCest
 
 		// First we create a model with fields.
 		$i->loginAsAdmin();
-		$i->haveContentModel('goose', 'geese', 'Geese go honk');
+		$i->haveContentModel('moose', 'mooses', 'Mooses go frmh');
 		$i->wait(1);
 	}
 
@@ -28,12 +28,12 @@ class PublishModelCest
 		$i->wait(1);
 
 		// Next we create an entry for our new model.
-		$i->amOnPage('/wp-admin/edit.php?post_type=goose');
+		$i->amOnPage('/wp-admin/edit.php?post_type=moose');
 		$i->click('Add New', '.wrap');
 		$i->wait(1);
 
-		$i->fillField(['name' => 'atlas-content-modeler[goose][integer]'], '');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][decimal]'], '');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][integer]'], '');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][decimal]'], '');
 		$i->scrollTo('#submitdiv');
 
 		$i->click('Publish', '#publishing-action');
@@ -42,8 +42,8 @@ class PublishModelCest
 		$i->see('This field is required');
 		$i->wait(1);
 
-		$i->seeInField('atlas-content-modeler[goose][integer]', '');
-		$i->seeInField('atlas-content-modeler[goose][decimal]', '');
+		$i->seeInField('atlas-content-modeler[moose][integer]', '');
+		$i->seeInField('atlas-content-modeler[moose][decimal]', '');
 	}
 
 	public function error_is_triggered_for_invalid_value(\AcceptanceTester $i)
@@ -62,12 +62,12 @@ class PublishModelCest
 		$i->wait(1);
 
 		// Next we create an entry for our new model.
-		$i->amOnPage('/wp-admin/edit.php?post_type=goose');
+		$i->amOnPage('/wp-admin/edit.php?post_type=moose');
 		$i->click('Add New', '.wrap');
 		$i->wait(1);
 
-		$i->fillField(['name' => 'atlas-content-modeler[goose][integer]'], '');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][decimal]'], '.0');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][integer]'], '');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][decimal]'], '.0');
 		$i->scrollTo('#submitdiv');
 
 		$i->click('Publish', '#publishing-action');
@@ -76,8 +76,8 @@ class PublishModelCest
 		$i->see('The input is invalid');
 		$i->wait(1);
 
-		$i->seeInField('atlas-content-modeler[goose][integer]', '');
-		$i->seeInField('atlas-content-modeler[goose][decimal]', '.0');
+		$i->seeInField('atlas-content-modeler[moose][integer]', '');
+		$i->seeInField('atlas-content-modeler[moose][decimal]', '.0');
 	}
 
 	public function i_can_publish_a_model_entry(AcceptanceTester $i)
@@ -118,18 +118,18 @@ class PublishModelCest
 		$i->wait(1);
 
 		// Next we create an entry for our new model.
-		$i->amOnPage('/wp-admin/edit.php?post_type=goose');
+		$i->amOnPage('/wp-admin/edit.php?post_type=moose');
 		$i->click('Add New', '.wrap');
 		$i->wait(1);
 
-		$i->fillField(['name' => 'atlas-content-modeler[goose][color]'], 'Gray');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][age]'], '100');
-		$i->fillField(['name' => 'atlas-content-modeler[goose][dateOfBirth]'], '01/01/2021');
-		$i->checkOption('atlas-content-modeler[goose][fliesSouthForWinter]');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][color]'], 'Gray');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][age]'], '100');
+		$i->fillField(['name' => 'atlas-content-modeler[moose][dateOfBirth]'], '01/01/2021');
+		$i->checkOption('atlas-content-modeler[moose][fliesSouthForWinter]');
 
 		// Rich text fields rendered as TinyMCE live in an iframe.
 		$i->switchToIFrame('#field-description iframe');
-		$i->fillField('#tinymce', 'I am a goose');
+		$i->fillField('#tinymce', 'I am a moose');
 		$i->switchToIFrame(); // switch back to main window
 
 		// Fill the second TinyMCE field.
@@ -144,25 +144,25 @@ class PublishModelCest
 
 		$i->see('Post published.');
 		$i->wait(1);
-		$i->see('Edit goose'); // Page title should change from “Add goose” when published.
+		$i->see('Edit moose'); // Page title should change from “Add moose” when published.
 
-		$i->seeInField('atlas-content-modeler[goose][color]', 'Gray');
-		$i->seeInField('atlas-content-modeler[goose][age]', '100');
-		$i->seeInField('atlas-content-modeler[goose][dateOfBirth]', '2021-01-01');
-		$i->seeCheckboxIsChecked('atlas-content-modeler[goose][fliesSouthForWinter]');
+		$i->seeInField('atlas-content-modeler[moose][color]', 'Gray');
+		$i->seeInField('atlas-content-modeler[moose][age]', '100');
+		$i->seeInField('atlas-content-modeler[moose][dateOfBirth]', '2021-01-01');
+		$i->seeCheckboxIsChecked('atlas-content-modeler[moose][fliesSouthForWinter]');
 		$i->switchToIFrame('#field-description iframe');
-		$i->see('I am a goose'); // Sees the text in the TinyMCE iframe body.
+		$i->see('I am a moose'); // Sees the text in the TinyMCE iframe body.
 		$i->switchToIFrame();
 
 		// Show <textarea> elements hidden by TinyMCE so we can see them to check their values directly.
 		$i->executeJS("
-			var field = document.getElementsByName('atlas-content-modeler[goose][description]');
+			var field = document.getElementsByName('atlas-content-modeler[moose][description]');
 			field[0].removeAttribute('style');
-			var fieldTwo = document.getElementsByName('atlas-content-modeler[goose][anotherRichTextField]');
+			var fieldTwo = document.getElementsByName('atlas-content-modeler[moose][anotherRichTextField]');
 			fieldTwo[0].removeAttribute('style');
 		");
 
-		$i->seeInField('atlas-content-modeler[goose][description]', '<p>I am a goose</p>');
-		$i->seeInField('atlas-content-modeler[goose][anotherRichTextField]', '<p>I am another rich text field</p>');
+		$i->seeInField('atlas-content-modeler[moose][description]', '<p>I am a moose</p>');
+		$i->seeInField('atlas-content-modeler[moose][anotherRichTextField]', '<p>I am another rich text field</p>');
 	}
 }
