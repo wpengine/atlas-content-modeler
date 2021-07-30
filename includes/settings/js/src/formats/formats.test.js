463bd07a661848cdd1d7b494f0d253a532034674
Andrew Matthews <andrew.matthews@wpengine.com>
2021-07-30 14:30:19 -0400

    test: add test for post type slug formatter

--- includes/settings/js/src/formats/formats.test.js
+++ includes/settings/js/src/formats/formats.test.js
@@ -1,4 +1,4 @@
-import { toValidApiId } from ".";
+import { toValidApiId, toPostTypeSlug } from ".";
 
 describe("toValidApiId", () => {
 	const cases = [
@@ -19,5 +19,28 @@ describe("toValidApiId", () => {
 
 	test.each(cases)("toValidApiId(%s) should be %s", (input, expected) => {
 		expect(toValidApiId(input)).toEqual(expected);
+	});
+});
+
+describe("toPostTypeSlug", () => {
+	const cases = [
+		[",,,#!!!strip_punctuation???$...", "strip_punctuation"],
+		["⚠️⚠️⚠️strip_emoji🐇🐇🐇", "strip_emoji"],
+		["strip accented éîøü", "stripaccented"],
+		["strips    white    spaces", "stripswhitespaces"],
+		["     trim_white_space     ", "trim_white_space"],
+		["1allow_leading_number", "1allow_leading_number"],
+		["123allow_leading_numbers", "123allow_leading_numbers"],
+		["Lower_case_All_the_THINGS", "lower_case_all_the_things"],
+		["_allows_leading_underscore", "_allows_leading_underscore"],
+		["-Allows-Hypens-", "-allows-hypens-"],
+		[
+			"allow numbers after 1st character123",
+			"allownumbersafter1stcharacter123",
+		],
+	];
+
+	test.each(cases)("toPostTypeSlug(%s) should be %s", (input, expected) => {
+		expect(toPostTypeSlug(input)).toEqual(expected);
 	});
 });
