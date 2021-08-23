a3e83765fc70f7830f9854d359e9f3963a5c4520
Nick Cernis <nick@cern.is>
2021-08-23 18:34:15 +0200

    feat: add entry options and “Remove” link
    Work in progress. Needs some styling.

--- includes/publisher/js/src/components/relationship/Entries.jsx
+++ includes/publisher/js/src/components/relationship/Entries.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Options from "./Options";
 
 export default function Entries({
 	entryInfo,
 	modelSlug,
 	field,
 	selectedEntries,
+	setSelectedEntries,
 }) {
 	return (
 		<section className="card-content">
@@ -12,13 +14,22 @@ export default function Entries({
 				{entryInfo?.map((entry) => {
 					const { title } = entry;
 					return (
-						<li key={entry.id}>
+						<li
+							key={entry.id}
+							className="d-flex flex-column d-sm-flex flex-sm-row justify-content-start"
+						>
 							<div className="relation-model-card flex-wrap d-flex flex-column d-sm-flex flex-sm-row">
 								<span className="flex-item mb-3 mb-sm-0 pr-1">
 									<p className="value">
 										<strong>{title.rendered}</strong>
 									</p>
 								</span>
+							</div>
+							<div className="order-0 my-2 my-sm-0 text-end order-sm-2 ms-sm-auto">
+								<Options
+									entry={entry}
+									setSelectedEntries={setSelectedEntries}
+								/>
 							</div>
 						</li>
 					);
--- includes/publisher/js/src/components/relationship/index.jsx
+++ includes/publisher/js/src/components/relationship/index.jsx
@@ -86,6 +86,7 @@ export default function Relationship({ field, modelSlug }) {
 					modelSlug={modelSlug}
 					field={field}
 					selectedEntries={selectedEntries}
+					setSelectedEntries={setSelectedEntries}
 				/>
 			) : (
 				"" // No linked entries.
