92ccf26d967fea8ad8a9be76062854462e3c6a81
Matthew Wright <matthew.wright@wpengine.com>
2021-08-23 10:36:12 -0500

    per PR

--- includes/publisher/js/src/App.jsx
+++ includes/publisher/js/src/App.jsx
@@ -8,20 +8,6 @@ const GA_ID = "G-S056CLLZ34";
 /**
  * React-GA4 Library and usage
  * {@link https://github.com/PriceRunner/react-ga4#readme}
- *
- * Page send
- * ReactGA.send("pageview"); OR
- * ReactGA.send({ hitType: "pageview", page: "/my-path" });
- *
- * Send custom event
- * ReactGA.event({
- *	category: "your category",
- *	action: "your action",
- *	label: "your label", // optional
- *	value: 99, // optional, must be a number
- *	nonInteraction: true, // optional, true/fals
- *	transport: "xhr", // optional, beacon/xhr/image
- *	});
  */
 ReactGA.initialize(GA_ID, { gtagOptions: { anonymize_ip: true } });
 
