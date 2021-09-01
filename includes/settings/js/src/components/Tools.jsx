e790e8e87127631398ce9ecd6c665c9ffe9397f0
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2021-09-01 12:23:07 -0500

    BH-1196: add tools to submenu and placeholder page (#260)
    * Added tools to submenu for ACM as well as the Tools page placeholder.

---
+++ includes/settings/js/src/components/Tools.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { __, sprintf } from "@wordpress/i18n";
+import { ModelsContext } from "../ModelsContext";
+
+export default function Tools() {
+	const { tools } = useContext(ModelsContext);
+	const history = useHistory();
+
+	const cancelEditing = () => {
+		history.push(atlasContentModeler.appPath + "&view=tools");
+	};
+
+	return (
+		<div className="app-card tools-view">
+			<section className="heading flex-wrap d-flex flex-column d-sm-flex flex-sm-row">
+				<h2>{__("Tools", "atlas-content-modeler")}</h2>
+			</section>
+			<section className="card-content">
+				<div className="row">
+					<div className="col-xs-10 col-lg-4 order-1 order-lg-0">
+						Tools Body
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+}
