d9b37262dd242b38f29e8ce8f1b75d3680280b8e
Nick Cernis <nick@cern.is>
2021-09-01 13:04:22 +0200

    chore: adjust delete model modal message
    - Show side-effects as a list.
    - Conditionally show a red warning if relationship fields
       will be affected. (No warning if the model is not
       referenced in a relationship field.)

--- includes/settings/js/src/components/DeleteModelModal.jsx
+++ includes/settings/js/src/components/DeleteModelModal.jsx
@@ -3,13 +3,14 @@ import Modal from "react-modal";
 import { ModelsContext } from "../ModelsContext";
 import { sprintf, __ } from "@wordpress/i18n";
 import { removeSidebarMenuItem } from "../utils";
+import { hasReferences } from "../queries";
 import { showError } from "../toasts";
 import { useHistory } from "react-router-dom";
 
 const { apiFetch } = wp;
 
 export function DeleteModelModal({ modalIsOpen, setModalIsOpen, model }) {
-	const { dispatch, taxonomiesDispatch } = useContext(ModelsContext);
+	const { models, dispatch, taxonomiesDispatch } = useContext(ModelsContext);
 	const { plural, slug } = model;
 	const history = useHistory();
 
@@ -63,20 +64,44 @@ export function DeleteModelModal({ modalIsOpen, setModalIsOpen, model }) {
 				)}
 			</h2>
 			<p>
-				{__(
-					"This is an irreversible action. You will have to recreate this model if you delete it.",
-					"atlas-content-modeler"
-				)}
+				<strong>
+					{__(
+						"This is an irreversible action.",
+						"atlas-content-modeler"
+					)}
+				</strong>
 			</p>
-			<p>
-				{__(
-					"This will NOT delete actual data stored in this model. It only deletes the model definition.",
-					"atlas-content-modeler"
+			<ul>
+				{hasReferences(models, slug) && (
+					<li className="warning">
+						{sprintf(
+							__(
+								// translators: plural form of the model name, such as “Cars”.
+								"Links will be removed in relationship fields that reference %s.",
+								"atlas-content-modeler"
+							),
+							plural
+						)}
+					</li>
 				)}
-			</p>
+				<li>
+					{__(
+						"The model definition will be deleted.",
+						"atlas-content-modeler"
+					)}
+				</li>
+				<li>
+					{__(
+						"Entries for this model will NOT be deleted.",
+						"atlas-content-modeler"
+					)}
+				</li>
+			</ul>
+
 			<p>
 				{sprintf(
 					__(
+						// translators: plural form of the model name, such as “Cars”.
 						"Are you sure you want to delete the %s content model?",
 						"atlas-content-modeler"
 					),
@@ -88,7 +113,7 @@ export function DeleteModelModal({ modalIsOpen, setModalIsOpen, model }) {
 				onClick={async () => {
 					// Optimistically remove the model from the UI.
 					dispatch({ type: "removeModel", slug });
-					// delete model and remove related menu sidebar item
+
 					await deleteModel(slug)
 						.then((res) => {
 							if (res.success) {
