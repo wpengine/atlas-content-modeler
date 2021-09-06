78682297044849fd61501475674c616a012aabe2
Nick Cernis <nick@cern.is>
2021-09-06 21:48:34 +0200

    wip: intercept trash link clicks
    Proof of concept. Using `confirm()` for now but intend to swap this
    with a React Modal.

--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./../../scss/index.scss";
+import { postSubmittedListener, moveToTrashListener } from "./listeners";
 
 const { models, postType } = atlasContentModelerFormEditingExperience;
 const container = document.getElementById("atlas-content-modeler-fields-app");
@@ -16,15 +17,6 @@ if (container && models.hasOwnProperty(postType)) {
 		container
 	);
 
-	/**
-	 * Allows styling of :invalid input fields only when the form was
-	 * submitted. Prevents an issue where error messages appear for
-	 * required fields when the form is first loaded.
-	 */
-	const form = document.querySelector("form#post");
-	const publishButton = document.querySelector("input#publish");
-	const addSubmittedClass = () => form.classList.add("submitted");
-
-	publishButton.addEventListener("click", addSubmittedClass);
-	form.addEventListener("submit", addSubmittedClass);
+	postSubmittedListener();
+	moveToTrashListener();
 }
