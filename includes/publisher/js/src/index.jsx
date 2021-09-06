2af0dfcf778f6acd78ae2177f69d0de6c4caa1c8
Nick Cernis <nick@cern.is>
2021-09-06 23:52:32 +0200

    Revert "wip: intercept trash link clicks"
    This reverts commit 78682297044849fd61501475674c616a012aabe2.

--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./../../scss/index.scss";
-import { postSubmittedListener, moveToTrashListener } from "./listeners";
 
 const { models, postType } = atlasContentModelerFormEditingExperience;
 const container = document.getElementById("atlas-content-modeler-fields-app");
@@ -17,6 +16,15 @@ if (container && models.hasOwnProperty(postType)) {
 		container
 	);
 
-	postSubmittedListener();
-	moveToTrashListener();
+	/**
+	 * Allows styling of :invalid input fields only when the form was
+	 * submitted. Prevents an issue where error messages appear for
+	 * required fields when the form is first loaded.
+	 */
+	const form = document.querySelector("form#post");
+	const publishButton = document.querySelector("input#publish");
+	const addSubmittedClass = () => form.classList.add("submitted");
+
+	publishButton.addEventListener("click", addSubmittedClass);
+	form.addEventListener("submit", addSubmittedClass);
 }
