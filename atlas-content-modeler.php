0481d641d1c7781c820f7cc24dddd3ca6b787d30
John Parris <john.parris@wpengine.com>
2021-09-08 13:53:27 -0400

    chore: update required WP version to 5.7

--- atlas-content-modeler.php
+++ atlas-content-modeler.php
@@ -8,7 +8,7 @@
  * Text Domain: atlas-content-modeler
  * Domain Path: /languages
  * Version: 0.5.0
- * Requires at least: 5.2
+ * Requires at least: 5.7
  * Requires PHP: 7.2
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
