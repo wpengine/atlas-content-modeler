d8771c4371496d03fc763dee62703ae57884f292
John Parris <john.parris@wpengine.com>
2021-09-15 10:34:57 -0400

    Add REST route for fetching all model schemas (#254)
    * test: add tests for fetching all models
    
    * feat: add REST endpoint for fetching all models

--- includes/rest-api/rest-api-endpoint-registration.php
+++ includes/rest-api/rest-api-endpoint-registration.php
@@ -23,6 +23,21 @@ add_action( 'rest_api_init', __NAMESPACE__ . '\register_rest_routes' );
  * Registers custom routes with the WP REST API.
  */
 function register_rest_routes(): void {
+	// Route for retrieving all models.
+	register_rest_route(
+		'wpe',
+		'/atlas/content-models/',
+		[
+			'methods'             => 'GET',
+			'callback'            => static function () {
+				return get_registered_content_types();
+			},
+			'permission_callback' => static function () {
+				return current_user_can( 'manage_options' );
+			},
+		]
+	);
+
 	// Route for retrieving a single content type.
 	register_rest_route(
 		'wpe',
