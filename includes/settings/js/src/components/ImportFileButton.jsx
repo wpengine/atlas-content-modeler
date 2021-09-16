0623ea59a904e498fdb4232b5070fc188f84e5f1
Matthew Wright <matthew.wright@wpengine.com>
2021-09-16 14:14:44 -0500

    Update ImportFileButton.jsx

--- includes/settings/js/src/components/ImportFileButton.jsx
+++ includes/settings/js/src/components/ImportFileButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { __ } from "@wordpress/i18n";
 import { showSuccess, showError } from "../toasts";
 
@@ -8,21 +8,59 @@ export default function ImportFileButton({
 	buttonTitle,
 	callbackFn,
 	buttonClasses,
-	allowedFileTypes,
+	allowedMimeTypes,
 }) {
+	const [file, setFile] = useState(null);
+	const fileUploaderRef = useRef(null);
+
+	/**
+	 * Import file click handler
+	 * @param event
+	 */
 	function importClickHandler(event) {
 		event.preventDefault();
-		console.log(event);
+		fileUploaderRef.current.click();
+	}
+
+	/**
+	 * Validate file before uploading
+	 * @param uploadedFile
+	 */
+	function validateFileUpload(uploadedFile) {
+		setFile(uploadedFile);
+	}
+
+	/**
+	 * Handle file change
+	 * @param event
+	 */
+	function onChangeHandler(event) {
+		event.preventDefault();
+		const uploadedFile = event.target.files[0];
+		if (uploadedFile) {
+			validateFileUpload(uploadedFile);
+		}
 	}
 
 	return (
-		<button
-			className={
-				buttonClasses || "button dark-blue button-primary link-button"
-			}
-			onClick={(event) => importClickHandler(event)}
-		>
-			{__(buttonTitle || "Select File", "atlas-content-modeler")}
-		</button>
+		<>
+			<input
+				type="file"
+				id="file"
+				ref={fileUploaderRef}
+				className="hidden"
+				onChange={onChangeHandler}
+			/>
+
+			<button
+				className={
+					buttonClasses ||
+					"button dark-blue button-primary link-button"
+				}
+				onClick={(event) => importClickHandler(event)}
+			>
+				{__(buttonTitle || "Select File", "atlas-content-modeler")}
+			</button>
+		</>
 	);
 }
