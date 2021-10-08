47fb5eb6435f1be90be44e43780dde4bab95f54e
Nick Cernis <nick@cern.is>
2021-10-08 16:03:08 +0200

    [NOJIRA] Fix the model icon picker to appear above the edit model modal (#313)
    * fix: put model icon picker above edit model modal
    
    * test: check model icon can be updated

--- tests/acceptance/EditContentModelCest.php
+++ tests/acceptance/EditContentModelCest.php
@@ -1,15 +1,12 @@
 <?php
 class EditContentModelCest {
 	public function i_can_update_an_existing_content_model( AcceptanceTester $I ) {
+		$I->resizeWindow(1024, 1024);
 		$I->loginAsAdmin();
+
 		// First, create a new model.
 		$I->haveContentModel('Candy', 'Candies');
 		$I->wait(1);
-
-		// Modal content is too large for default size
-		// which causes modal title to not be visible
-		// and the link in #wpfooter to overlay Save button.
-		$I->resizeWindow(1024, 1024);
 
 		// Invoke edit mode.
 		$I->amOnWPEngineContentModelPage();
@@ -17,11 +14,16 @@ class EditContentModelCest {
 		$I->click( '.dropdown-content a.edit' );
 		$I->see( 'Edit Candies' );
 
-		// Update the model data and save.
+		// Update the model data.
 		$I->fillField(['name' => 'singular'], 'Cat');
 		$I->fillField(['name' => 'plural'], 'Cats');
 		$I->fillField(['name' => 'description'], 'Cats are better than candy.');
 		$I->see('27/250', 'span.count');
+
+		// Change the model's icon.
+		$I->click('.dashicons-picker');
+		$I->waitForElement('.dashicon-picker-container');
+		$I->click('.dashicon-picker-container .dashicons-admin-media');
 
 		$I->click('Save');
 
@@ -29,5 +31,9 @@ class EditContentModelCest {
 		$I->wait(1);
 		$I->see('Cats', '.model-list');
 		$I->see('Cats are better than candy.', '.model-list');
+
+		// Check the icon in the WP admin sidebar was updated.
+		$classes = $I->grabAttributeFrom('#menu-posts-candy .wp-menu-image', 'class');
+		$I->assertContains('dashicons-admin-media', $classes);
 	}
 }
