01e4ffb4dde354da925ba51cfe7ee45c99d7095a
Chris Wiegman <chris.wiegman@wpengine.com>
2021-11-18 10:56:43 -0500

    Create item with featured image

--- tests/acceptance/CreateContentModelMediaFieldCest.php
+++ tests/acceptance/CreateContentModelMediaFieldCest.php
@@ -21,4 +21,25 @@ class CreateContentModelMediaFieldCest {
 		$i->see( 'Media', '.field-list div.type' );
 		$i->see( 'Product Photo', '.field-list div.widest' );
 	}
+
+	/**
+	 * Ensure a user can add a featured image media field to the model and see it within the list.
+	 */
+	public function i_can_add_a_featured_imagemedia_field_to_a_content_model( AcceptanceTester $i ) {
+		$i->loginAsAdmin();
+		$i->haveContentModel( 'Candy', 'Candies' );
+		$i->wait( 1 );
+
+		$i->click( 'Media', '.field-buttons' );
+		$i->wait( 1 );
+		$i->fillField( [ 'name' => 'name' ], 'The Primary Image' );
+		$i->see( '17/50', 'span.count' );
+		$i->seeInField( '#slug', 'thePrimaryImage' );
+		$i->checkOption( 'isFeatured' );
+		$i->click( '.open-field button.primary' );
+		$i->wait( 1 );
+
+		$i->see( 'Media', '.field-list div.type' );
+		$i->see( 'The Primary Image', '.field-list div.widest' );
+	}
 }
