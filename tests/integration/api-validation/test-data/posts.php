6809135383859286c6825e69833a176951b6efc5
Chris Wiegman <chris.wiegman@wpengine.com>
2021-11-18 09:47:16 -0500

    Add featured image test post

--- tests/integration/api-validation/test-data/posts.php
+++ tests/integration/api-validation/test-data/posts.php
@@ -90,6 +90,8 @@ function populate_post( $post_id, $model, $test_class ) {
 	update_post_meta( $post_id, 'mediaPDF', $ids[ $model . '_pdf_id' ] );
 	update_post_meta( $post_id, 'boolean', 'false' );
 	update_post_meta( $post_id, 'booleanRequired', 'true' );
+	update_post_meta( $post_id, 'featured', $ids[ $model . '_image_id' ] );
+	update_post_meta( $post_id, '_thumbnail_id', $ids[ $model . '_image_id' ] );
 
 	$image_meta = array(
 		'width'  => 1000,
