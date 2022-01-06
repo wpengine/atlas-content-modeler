f66454d28f2f5396a25bbfde040b4db5d63e772b
Matthew Wright <matthew.wright@wpengine.com>
2022-01-06 11:19:21 -0600

    Update Settings.jsx

--- includes/settings/js/src/components/Settings.jsx
+++ includes/settings/js/src/components/Settings.jsx
@@ -5,7 +5,9 @@ const { apiFetch } = wp;
 
 export default function Settings() {
 	function onChangeValue(event) {
-		alert(event.target.value);
+		if (event.target.checked) {
+			alert(event.target.value);
+		}
 	}
 
 	return (
@@ -14,33 +16,37 @@ export default function Settings() {
 				<h2>{__("Settings", "atlas-content-modeler")}</h2>
 			</section>
 			<section className="card-content">
-				<div className="row">
-					<div className="col-xs-10 col-lg-4 order-1 order-lg-0">
-						<div className="row">
-							<div className="col-xs-12">
-								<h4>
-									{__("Analytics", "atlas-content-modeler")}
-								</h4>
-								<p className="help">
-									{__(
-										"Opt into anonymous usage tracking to help us make Atlas Content Modeler better.",
-										"atlas-content-modeler"
-									)}
-								</p>
-								<div onChange={this.onChangeValue}>
+				<form>
+					<div className="row">
+						<div className="col-xs-10 col-lg-4 order-1 order-lg-0">
+							<div className="row">
+								<div className="col-xs-12">
+									<h4>
+										{__(
+											"Analytics",
+											"atlas-content-modeler"
+										)}
+									</h4>
+									<p className="help">
+										{__(
+											"Opt into anonymous usage tracking to help us make Atlas Content Modeler better.",
+											"atlas-content-modeler"
+										)}
+									</p>
 									<div className="row">
 										<div className="col-xs-12">
-											<input
-												type="radio"
-												id="optin"
-												name="optin"
-												value="false"
-												defaultChecked
-											/>
 											<label
 												className="radio"
 												htmlFor="optin"
 											>
+												<input
+													type="radio"
+													id="optin"
+													name="optin"
+													value="false"
+													defaultChecked
+													onChange={onChangeValue}
+												></input>
 												{__(
 													"Disabled",
 													"atlas-content-modeler"
@@ -53,14 +59,14 @@ export default function Settings() {
 												id="optin"
 												name="optin"
 												value="true"
-											/>
+												onChange={onChangeValue}
+											></input>
 											<label
 												className="radio"
 												htmlFor="optin"
 											>
 												{__(
 													"Enabled",
-
 													"atlas-content-modeler"
 												)}
 											</label>
@@ -70,7 +76,7 @@ export default function Settings() {
 							</div>
 						</div>
 					</div>
-				</div>
+				</form>
 			</section>
 		</div>
 	);
