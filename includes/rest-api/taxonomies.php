f4eb79d2d24caa00ccb33831e40b7d32c6eb9071
Nick Cernis <nick@cern.is>
2022-01-10 13:36:48 +0100

    chore: adjust taxonomy label collision logic
    - Use new root_type_exists() function.
    - Return custom singular and plural label errors so that we can display them to the user.

--- includes/rest-api/taxonomies.php
+++ includes/rest-api/taxonomies.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace WPE\AtlasContentModeler\REST_API\Taxonomies;
 
 use WP_Error;
-use function WPE\AtlasContentModeler\ContentRegistration\get_registered_content_types;
+use function WPE\AtlasContentModeler\REST_API\GraphQL\root_type_exists;
 
 /**
  * Saves a taxonomy.
@@ -21,19 +21,24 @@ use function WPE\AtlasContentModeler\ContentRegistration\get_registered_content_
  * @since 0.6.0
  */
 function save_taxonomy( array $params, bool $is_update ) {
-	// Get models for checking slug, plural and singular labels.
-	$content_types = get_registered_content_types();
-
 	// Sanitize key allows hyphens, but it's close enough to register_taxonomy() requirements.
 	$params['slug']     = isset( $params['slug'] ) ? sanitize_key( $params['slug'] ) : '';
-	$params['singular'] = isset( $params['singular'] ) ? sanitize_key( $params['singular'] ) : '';
-	$params['plural']   = isset( $params['plural'] ) ? sanitize_key( $params['plural'] ) : '';
+	$params['singular'] = isset( $params['singular'] ) ? $params['singular'] : '';
+	$params['plural']   = isset( $params['plural'] ) ? $params['plural'] : '';
 	$reserved_tax_terms = include ATLAS_CONTENT_MODELER_INCLUDES_DIR . 'settings/reserved-taxonomy-terms.php';
 
 	if ( empty( $params['slug'] ) || strlen( $params['slug'] ) > 32 ) {
 		return new WP_Error(
 			'acm_invalid_taxonomy_id',
 			esc_html__( 'Taxonomy slug must be between 1 and 32 characters in length.', 'atlas-content-modeler' ),
+			[ 'status' => 400 ]
+		);
+	}
+
+	if ( empty( $params['singular'] ) || empty( $params['plural'] ) ) {
+		return new WP_Error(
+			'acm_invalid_labels',
+			esc_html__( 'Please provide singular and plural labels when creating a taxonomy.', 'atlas-content-modeler' ),
 			[ 'status' => 400 ]
 		);
 	}
@@ -52,7 +57,7 @@ function save_taxonomy( array $params, bool $is_update ) {
 	$non_acm_taxonomies = array_diff( array_keys( $wp_taxonomies ), array_keys( $acm_taxonomies ) );
 
 	// Prevents creation of a taxonomy if one with the same slug exists that was not created in ACM.
-	if ( in_array( $params['slug'], $non_acm_taxonomies, true ) || in_array( $params['slug'], $content_types, true ) ) {
+	if ( in_array( $params['slug'], $non_acm_taxonomies, true ) ) {
 		return new WP_Error(
 			'acm_taxonomy_exists',
 			esc_html__( 'A taxonomy with this Taxonomy ID already exists.', 'atlas-content-modeler' ),
@@ -60,8 +65,8 @@ function save_taxonomy( array $params, bool $is_update ) {
 		);
 	}
 
-	// Allows updates of existing ACM taxonomies, but prevents creation of ACM taxonomies with identical slugs and models.
-	if ( ! $is_update && array_key_exists( $params['slug'], $acm_taxonomies ) || in_array( $params['slug'], $content_types, true ) ) {
+	// Allows updates of existing ACM taxonomies, but prevents creation of ACM taxonomies with identical slugs.
+	if ( ! $is_update && array_key_exists( $params['slug'], $acm_taxonomies ) ) {
 		return new WP_Error(
 			'acm_taxonomy_exists',
 			esc_html__( 'A taxonomy with this Taxonomy ID already exists.', 'atlas-content-modeler' ),
@@ -69,22 +74,30 @@ function save_taxonomy( array $params, bool $is_update ) {
 		);
 	}
 
-	// Check for label conflicts in taxonomies and models.
-	if ( ! $is_update && ( array_key_exists( $params['plural'], $acm_taxonomies ) || array_key_exists( $params['singular'], $acm_taxonomies ) || in_array( $params['singular'], $content_types, true ) || in_array( $params['plural'], $content_types, true ) ) ) {
-		return new WP_Error(
-			'acm_taxonomy_exists',
-			esc_html__( 'A taxonomy with this Taxonomy label already exists.', 'atlas-content-modeler' ),
-			[ 'status' => 400 ]
-		);
+	if (
+		! $is_update ||
+		( $is_update && taxonomy_property_changed( $params['slug'], 'singular', $params['singular'] ) )
+	) {
+		if ( root_type_exists( $params['singular'] ) ) {
+			return new WP_Error(
+				'acm_singular_label_exists',
+				esc_html__( 'The singular label is already in use.', 'atlas-content-modeler' ),
+				[ 'status' => 400 ]
+			);
+		}
 	}
 
-	// Check for required singular and plural params.
-	if ( empty( $params['singular'] ) || empty( $params['plural'] ) ) {
-		return new WP_Error(
-			'acm_invalid_labels',
-			esc_html__( 'Please provide singular and plural labels when creating a taxonomy.', 'atlas-content-modeler' ),
-			[ 'status' => 400 ]
-		);
+	if (
+		! $is_update ||
+		( $is_update && taxonomy_property_changed( $params['slug'], 'plural', $params['plural'] ) )
+	) {
+		if ( root_type_exists( $params['plural'] ) ) {
+			return new WP_Error(
+				'acm_plural_label_exists',
+				esc_html__( 'The plural label is already in use.', 'atlas-content-modeler' ),
+				[ 'status' => 400 ]
+			);
+		}
 	}
 
 	$defaults = [
@@ -107,4 +120,21 @@ function save_taxonomy( array $params, bool $is_update ) {
 	}
 
 	return $taxonomy;
+}
+
+/**
+ * Determines if a new taxonomy property value differs from the old one.
+ *
+ * Used for extra validation against modified properties, such as checking that
+ * an updated singular label does not conflict with root GraphQL fields.
+ *
+ * @param string $slug The taxonomy ID.
+ * @param string $property The property to check.
+ * @param mixed  $new_value The property's new value.
+ * @return bool
+ */
+function taxonomy_property_changed( string $slug, string $property, $new_value ): bool {
+	$acm_taxonomies = get_option( 'atlas_content_modeler_taxonomies', array() );
+
+	return ( $acm_taxonomies[ $slug ][ $property ] ?? null ) !== $new_value;
 }
