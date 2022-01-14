39b2b4eb4d2610cfbf1b3db577e09f320f13e07a
John Parris <john.parris@wpengine.com>
2022-01-14 14:02:12 -0500

    chore: DRY

--- tests/acceptance/UsageTrackingCest.php
+++ tests/acceptance/UsageTrackingCest.php
@@ -2,16 +2,14 @@
 class UsageTrackingCest {
 	public function _before( AcceptanceTester $i ) {
 		$i->loginAsAdmin();
+		$i->amOnPluginSettingsPage();
 	}
 
 	public function i_can_see_the_usage_tracking_setting( AcceptanceTester $i ) {
-		$i->amOnPluginSettingsPage();
 		$i->see( 'Opt into anonymous usage tracking to help us make Atlas Content Modeler better' );
 	}
 
 	public function i_can_toggle_the_setting_and_see_the_value_updated_in_the_database( AcceptanceTester $i ) {
-		$i->amOnPluginSettingsPage();
-
 		// Enable the setting.
 		$i->click( '#atlas-content-modeler-settings[usageTrackingEnabled]' );
 		$i->wait( 1 ); // wait for API call that saves setting to db.
