15607728bb7265b3ff971eb4c7185f08365f7a66
Nick Cernis <nick@cern.is>
2022-01-19 12:46:27 +0100

    [NOJIRA] Ensure rich text fields load consistently for publishers (#400)
    * fix: ensure rich text fields load correctly
    
    Fixes #399 by making sure wp.oldEditor.getDefaultSettings is available
    before using it to initialize the Rich Text field.
    
    * docs: add changelog entry

--- includes/publisher/js/src/components/RichTextEditor.jsx
+++ includes/publisher/js/src/components/RichTextEditor.jsx
@@ -1,37 +1,51 @@
 /* global atlasContentModelerFormEditingExperience */
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Icon from "../../../../components/icons";
 import { __ } from "@wordpress/i18n";
 const { wp } = window;
 
-export default function RichTextEditor({
-	field,
-	modelSlug,
-	errors,
-	validate,
-	defaultError,
-}) {
+export default function RichTextEditor({ field, modelSlug, defaultError }) {
 	const fieldId = `atlas-content-modeler-${modelSlug}-${field.slug}`;
+	const editorReadyTimer = useRef(null);
 
 	useEffect(() => {
+		const editorReadyTime = 500;
+		const initializeEditorWhenReady = () => {
+			/**
+			 * WP defines getDefaultSettings() in an admin footer script tag after
+			 * admin scripts are enqueued, so we must wait for it to be available.
+			 */
+			if (typeof wp?.oldEditor?.getDefaultSettings === "function") {
+				wp.oldEditor.initialize(fieldId, {
+					...wp.oldEditor.getDefaultSettings(),
+					tinymce: {
+						height: "300",
+						toolbar1:
+							"undo,redo,formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media",
+					},
+					mediaButtons: true,
+					quicktags: false,
+				});
+			} else {
+				editorReadyTimer.current = setTimeout(
+					initializeEditorWhenReady,
+					editorReadyTime
+				);
+			}
+		};
+
 		if (
 			atlasContentModelerFormEditingExperience?.models ||
 			atlasContentModelerFormEditingExperience?.models[
 				atlasContentModelerFormEditingExperience.postType
 			]
 		) {
-			wp.oldEditor.initialize(fieldId, {
-				...wp.oldEditor.getDefaultSettings(),
-				tinymce: {
-					height: "300",
-					toolbar1:
-						"undo,redo,formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media",
-				},
-				mediaButtons: true,
-				quicktags: false,
-			});
+			initializeEditorWhenReady();
 		}
-	}, []);
+		return () => {
+			clearTimeout(editorReadyTimer.current);
+		};
+	}, [fieldId, editorReadyTimer]);
 
 	return (
 		<>
