cfedbdac351b082d72d5d75d2cd6ddd75ac0d502
Nick Cernis <nick@cern.is>
2022-01-19 12:29:02 +0100

    refactor: prevent title field changes
    Once a field is set as the title field no other text field
    can be a title field until the original one is deleted.

--- includes/settings/js/src/components/fields/TextFields.jsx
+++ includes/settings/js/src/components/fields/TextFields.jsx
@@ -5,18 +5,21 @@ import React, { useContext, useRef } from "react";
 import { getTitleFieldId } from "../../queries";
 import { ModelsContext } from "../../ModelsContext";
 import { useLocationSearch } from "../../utils";
-import { sprintf, __ } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 
 const TextFields = ({ register, data, editing, fieldId }) => {
-	const { models, dispatch } = useContext(ModelsContext);
+	const { models } = useContext(ModelsContext);
 	const query = useLocationSearch();
 	const currentModel = query.get("id");
 	const fields = models[currentModel]?.fields;
 	const originalTitleFieldId = useRef(getTitleFieldId(fields));
+	const shouldShowTitleField =
+		!originalTitleFieldId.current ||
+		originalTitleFieldId.current === fieldId;
 
 	return (
 		<>
-			{!data?.parent && (
+			{shouldShowTitleField && (
 				<div className="field">
 					<legend>Title Field</legend>
 					<input
@@ -24,55 +27,8 @@ const TextFields = ({ register, data, editing, fieldId }) => {
 						type="checkbox"
 						id={`is-title-${fieldId}`}
 						ref={register}
-						checked={data?.isTitle === true}
-						onChange={(event) => {
-							/**
-							 * Unchecks other fields when checking a field.
-							 * Only one field can be the title field.
-							 */
-							if (event.target.checked) {
-								dispatch({
-									type: "setTitleField",
-									id: fieldId,
-									model: currentModel,
-								});
-								return;
-							}
-
-							if (!event.target.checked) {
-								/**
-								 * When unchecking a field that was not the original
-								 * title, restore isTitle on the original title
-								 * field if there is one. Prevents an issue where
-								 * checking “is title” then unchecking it removes
-								 * isTitle from the original.
-								 */
-								if (
-									originalTitleFieldId.current &&
-									originalTitleFieldId.current !== fieldId
-								) {
-									dispatch({
-										type: "setTitleField",
-										id: originalTitleFieldId.current,
-										model: currentModel,
-									});
-									return;
-								}
-
-								/**
-								 * At this point we're just unchecking the original
-								 * title field.
-								 */
-								dispatch({
-									type: "setFieldProperties",
-									id: fieldId,
-									model: currentModel,
-									properties: [
-										{ name: "isTitle", value: false },
-									],
-								});
-							}
-						}}
+						defaultChecked={data?.isTitle === true}
+						disabled={!!originalTitleFieldId.current}
 					/>
 					<label
 						htmlFor={`is-title-${fieldId}`}
