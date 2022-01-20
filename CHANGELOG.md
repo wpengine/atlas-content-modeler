6e623024d5c844a998ad90f9898eb03197005dad
Nick Cernis <nick@cern.is>
2022-01-20 09:49:55 +0100

    [MTKA-1371] Prevent title field changes (#404)
    * refactor: remove setTitleField reducer action
    
    We no longer need this because only one text field
    can be the title field. “Make title” is now not
    displayed on other fields if a model already has
    a title field.
    
    * refactor: prevent title field changes
    
    Once a field is set as the title field no other text field
    can be a title field until the original one is deleted.
    
    * docs: add changelog entry
    
    * test: second text field doesn't have title option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@
 ### Fixed
 - Ensure Rich Text fields load for publishers even if WordPress Core editor scripts are slow to execute.
 
+### Changed
+- The title field of a model can no longer be changed once set, unless you delete the original title field. This prepares upcoming work to save title field data to WordPress post titles, allowing title field content to be searchable.
+
 ## 0.12.1 - 2022-01-12
 ### Fixed
 - Prevent PHP fatal errors on sites running < PHP 7.4 under certain conditions when no models exist.
--- includes/settings/js/src/components/fields/TextFields.jsx
+++ includes/settings/js/src/components/fields/TextFields.jsx
@@ -1,22 +1,23 @@
 /**
  * Additional form fields for the Text field type.
  */
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import { getTitleFieldId } from "../../queries";
 import { ModelsContext } from "../../ModelsContext";
 import { useLocationSearch } from "../../utils";
-import { sprintf, __ } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 
 const TextFields = ({ register, data, editing, fieldId }) => {
-	const { models, dispatch } = useContext(ModelsContext);
+	const { models } = useContext(ModelsContext);
 	const query = useLocationSearch();
 	const currentModel = query.get("id");
 	const fields = models[currentModel]?.fields;
-	const originalTitleFieldId = useRef(getTitleFieldId(fields));
+	const titleFieldId = getTitleFieldId(fields);
+	const showTitleField = !titleFieldId || titleFieldId === fieldId;
 
 	return (
 		<>
-			{!data?.parent && (
+			{showTitleField && (
 				<div className="field">
 					<legend>Title Field</legend>
 					<input
@@ -24,55 +25,8 @@ const TextFields = ({ register, data, editing, fieldId }) => {
 						type="checkbox"
 						id={`is-title-${fieldId}`}
 						ref={register}
-						checked={data?.isTitle === true}
-						onChange={(event) => {
-							/**
-							 * Unchecks other fields when checking a field.
-							 * Only one field can be the title field.
-							 */
-							if (event.target.checked) {
-								dispatch({
-									type: "setTitleField",
-									id: fieldId,
-									model: currentModel,
-								});
-								return;
-							}
-
-							if (!event.target.checked) {
-								/**
-								 * When unchecking a field that was not the original
-								 * title, restore isTitle on the original title
-								 * field if there is one. Prevents an issue where
-								 * checking “is title” then unchecking it removes
-								 * isTitle from the original.
-								 */
-								if (
-									originalTitleFieldId.current &&
-									originalTitleFieldId.current !== fieldId
-								) {
-									dispatch({
-										type: "setTitleField",
-										id: originalTitleFieldId.current,
-										model: currentModel,
-									});
-									return;
-								}
-
-								/**
-								 * At this point we're just unchecking the original
-								 * title field.
-								 */
-								dispatch({
-									type: "setFieldProperties",
-									id: fieldId,
-									model: currentModel,
-									properties: [
-										{ name: "isTitle", value: false },
-									],
-								});
-							}
-						}}
+						defaultChecked={data?.isTitle === true}
+						disabled={!!titleFieldId}
 					/>
 					<label
 						htmlFor={`is-title-${fieldId}`}
