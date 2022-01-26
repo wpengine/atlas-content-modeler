e647b1f290db18a145c79711c3e53db5c956bd61
Matthew Wright <matthew.wright@wpengine.com>
2022-01-26 14:15:26 -0600

    basic css

--- includes/shared-assets/js/components/Dropdown.jsx
+++ includes/shared-assets/js/components/Dropdown.jsx
@@ -2,7 +2,56 @@
 import { css, jsx } from "@emotion/react";
 
 export function Dropdown(props) {
-	const cssAttributes = css``;
+	const cssAttributes = css`
+		position: relative;
+		min-height: 20px;
 
-	return <div css={cssAttributes} {...props} />;
+		.dropdown-content:not(.hidden) {
+			background: #fff;
+			border-radius: 2px;
+			box-shadow: 0px 5px 20px rgba(68, 68, 68, 0.15);
+			display: flex;
+			flex-direction: column;
+			height: auto;
+			justify-content: center;
+			padding: 10px 0;
+			position: absolute;
+			right: 0;
+			z-index: 100;
+
+			a {
+				color: #7e5cef;
+				display: block;
+				font-size: 15px;
+				line-height: 25px;
+				padding: 5px 20px;
+				text-align: center;
+				text-decoration: none;
+				white-space: nowrap;
+			}
+
+			a:hover {
+				color: #5c43ae;
+			}
+
+			a.delete {
+				color: #d21b46;
+			}
+
+			a.delete:hover {
+				color: #991433;
+			}
+		}
+
+		/* Adjust margin on smaller devices */
+		@media (max-width: 850px) {
+			.neg-margin-wrapper {
+				.dropdown {
+					margin-left: -80px;
+				}
+			}
+		}
+	`;
+
+	return <span css={cssAttributes} {...props} />;
 }
