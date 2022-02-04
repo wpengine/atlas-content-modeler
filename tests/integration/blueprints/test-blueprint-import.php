6e2ff229d1d37913f696341cf13863f5f7cb2805
Nick Cernis <nick@cern.is>
2022-02-04 12:06:51 +0100

    refactor: get good manifest before test runs

--- tests/integration/blueprints/test-blueprint-import.php
+++ tests/integration/blueprints/test-blueprint-import.php
@@ -26,20 +26,26 @@ use function WPE\AtlasContentModeler\REST_API\Models\create_models;
  * @covers WPE\AtlasContentModeler\Blueprint\Import
  */
 class BlueprintImportTest extends WP_UnitTestCase {
+
+	private $manifest;
+
+	public function setUp() {
+		parent::setUp();
+		$this->manifest = get_manifest( __DIR__ . '/test-data/blueprint-good' );
+	}
+
 	public function test_get_manifest_gives_manifest_data(): void {
-		$manifest = get_manifest( __DIR__ . '/test-data/blueprint-good' );
-		self::assertArrayHasKey( 'meta', $manifest );
+		self::assertArrayHasKey( 'meta', $this->manifest );
 	}
 
 	public function test_get_missing_manifest_gives_error(): void {
-		$manifest = get_manifest( __DIR__ . '/test-data/this-folder-does-not-exist' );
-		self::assertInstanceOf( 'WP_Error', $manifest );
-		self::assertSame( 'acm_manifest_error', $manifest->get_error_code() );
+		$bad_manifest = get_manifest( __DIR__ . '/test-data/this-folder-does-not-exist' );
+		self::assertInstanceOf( 'WP_Error', $bad_manifest );
+		self::assertSame( 'acm_manifest_error', $bad_manifest->get_error_code() );
 	}
 
 	public function test_check_versions_passes(): void {
-		$manifest = get_manifest( __DIR__ . '/test-data/blueprint-good' );
-		$check    = check_versions( $manifest );
+		$check = check_versions( $this->manifest );
 		self::assertTrue( $check );
 	}
 
@@ -66,19 +72,17 @@ class BlueprintImportTest extends WP_UnitTestCase {
 	}
 
 	public function test_import_taxonomies() {
-		$manifest = get_manifest( __DIR__ . '/test-data/blueprint-good' );
-		import_taxonomies( $manifest['taxonomies'] );
+		import_taxonomies( $this->manifest['taxonomies'] );
 
 		$taxonomies = get_option( 'atlas_content_modeler_taxonomies' );
 		self::assertArrayHasKey( 'breed', $taxonomies );
 	}
 
 	public function test_import_posts() {
-		$manifest            = get_manifest( __DIR__ . '/test-data/blueprint-good' );
-		$expected_post_count = count( $manifest['posts'] );
+		$expected_post_count = count( $this->manifest['posts'] );
 
-		create_models( $manifest['models'] );
-		import_posts( $manifest['posts'] );
+		create_models( $this->manifest['models'] );
+		import_posts( $this->manifest['posts'] );
 
 		$posts = get_posts( [ 'post_type' => 'rabbit' ] );
 		self::assertCount( $expected_post_count, $posts );
