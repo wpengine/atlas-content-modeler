61ae6addac40deaf6da8ca051fa4b913ae615cd8
John Parris <john.parris@wpengine.com>
2022-02-04 09:49:19 -0500

    test: confirm `get_remote_blueprint()` behavior when valid zip provided.
    refactor: add `get_test_blueprint_zip()` for getting zip file with blueprint test data.

--- tests/integration/blueprints/test-blueprint-downloads.php
+++ tests/integration/blueprints/test-blueprint-downloads.php
@@ -10,6 +10,9 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 		do_action( 'init' );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\get_remote_blueprint
+	 */
 	public function test_get_remote_blueprint_returns_WP_Error_when_an_empty_url_is_provided(): void {
 		add_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 		$response = get_remote_blueprint( '' );
@@ -17,6 +20,9 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 		remove_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\get_remote_blueprint
+	 */
 	public function test_get_remote_blueprint_returns_WP_Error_when_an_empty_response_is_encountered(): void {
 		add_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 		$response = get_remote_blueprint( 'http://emptyresponse' );
@@ -25,6 +31,9 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 		remove_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\get_remote_blueprint
+	 */
 	public function test_get_remote_blueprint_returns_WP_Error_when_an_empty_response_body_is_encountered(): void {
 		add_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 		$response = get_remote_blueprint( 'http://emptybody' );
@@ -32,6 +41,9 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 		remove_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\get_remote_blueprint
+	 */
 	public function test_get_remote_blueprint_returns_WP_Error_when_a_non_200_response_is_encountered(): void {
 		add_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 		$response = get_remote_blueprint( 'http://status404' );
@@ -40,18 +52,43 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 		remove_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\get_remote_blueprint
+	 */
+	public function test_get_remote_blueprint_returns_blueprint_zip_when_given_valid_url_to_zip(): void {
+		add_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
+		$response = get_remote_blueprint( 'http://valid' );
+		$this->assertSame( $response, $this->get_test_blueprint_zip() );
+		remove_filter( 'pre_http_request', [ $this, 'filter_wp_remote_get_return_value' ], 10, 3 );
+	}
+
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\save_blueprint_to_upload_dir
+	 */
 	public function test_save_blueprint_to_upload_dir_returns_WP_Error_when_an_invalid_blueprint_file_is_encountered(): void {
 		$saved = save_blueprint_to_upload_dir( 'invalid file', 'test.zip' );
 		$this->assertWPError( $saved );
 		self::assertSame( 'acm_blueprint_unsupported_file_type', $saved->get_error_code() );
 	}
 
+	/**
+	 * @covers ::WPE\AtlasContentModeler\Blueprint\Fetch\save_blueprint_to_upload_dir
+	 */
 	public function test_save_blueprint_to_upload_dir_saves_file_when_give_a_valid_blueprint(): void {
-		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
-		$blueprint = file_get_contents( __DIR__ . '/test-data/acm-rabbits.zip' );
+		$blueprint = $this->get_test_blueprint_zip();
 		$saved     = save_blueprint_to_upload_dir( $blueprint, 'acm-rabbits.zip' );
 		self::assertTrue( $saved );
 		self::assertTrue( is_readable( trailingslashit( wp_upload_dir()['path'] ) . 'acm-rabbits.zip' ) );
+	}
+
+	/**
+	 * Returns test blueprint data.
+	 *
+	 * @return string
+	 */
+	protected function get_test_blueprint_zip(): string {
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		return file_get_contents( __DIR__ . '/test-data/acm-rabbits.zip' );
 	}
 
 	/**
@@ -77,6 +114,13 @@ class TestBlueprintDownloadTestCases extends WP_UnitTestCase {
 						'message' => 'Not found',
 					],
 					'status'   => 404,
+				];
+			case 'http://valid':
+				return [
+					'body'     => $this->get_test_blueprint_zip(),
+					'response' => [
+						'code' => 200,
+					],
 				];
 			default:
 				return $preempt;
