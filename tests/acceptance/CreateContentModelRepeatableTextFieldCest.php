001ec6176684f87a59c9a94b8cebf91286c75b91
Ryan Meier <ryan.meier@wpengine.com>
2022-02-23 13:06:28 -0600

    Added end-2-end tests for repeatable text field minimum and maximum cases

--- tests/acceptance/CreateContentModelRepeatableTextFieldCest.php
+++ tests/acceptance/CreateContentModelRepeatableTextFieldCest.php
@@ -3,7 +3,7 @@
 class CreateContentModelRepeatableTextFieldCest {
 
 	public function _before( \AcceptanceTester $i ) {
-		$i->maximizeWindow();
+		$i->resizeWindow( 1280, 1024 );
 		$i->loginAsAdmin();
 		$content_model = $i->haveContentModel( 'Candy', 'Candies' );
 		$i->amOnWPEngineEditContentModelPage( $content_model['slug'] );
@@ -111,7 +111,80 @@ class CreateContentModelRepeatableTextFieldCest {
 		$i->seeElement( 'button.add-option' );
 	}
 
-	public function i_can_create_a_content_model_text_repeatable_field_with_a_max_of_three( AcceptanceTester $i ) {
+	public function a_non_required_repeatable_text_field_with_a_min_of_two_does_not_require_all_minimum_fields_to_be_present( AcceptanceTester $i ) {
+		$i->click( 'button[data-testid="edit-model-update-create-settings-button"]' );
+		$i->fillField( [ 'name' => 'minRepeatable' ], '2' );
+		$i->click( 'button[data-testid="model-advanced-settings-done-button"]' );
+		$i->wait( 1 );
+		$i->click( 'button[data-testid="edit-model-update-create-button"]' );
+		$i->wait( 1 );
+
+		$i->amOnPage( '/wp-admin/edit.php?post_type=candy' );
+		$i->click( 'Add New', '.wrap' );
+		$i->wait( 1 );
+
+		$i->click( 'Publish', '#publishing-action' );
+		$i->wait( 1 );
+		$i->see( 'Post published' );
+	}
+
+	public function a_non_required_repeatable_text_field_with_a_minimum_will_trigger_validation_if_minimum_valid_fields_are_not_present( AcceptanceTester $i ) {
+		$i->click( 'button[data-testid="edit-model-update-create-settings-button"]' );
+		$i->fillField( [ 'name' => 'minRepeatable' ], '2' );
+		$i->click( 'button[data-testid="model-advanced-settings-done-button"]' );
+		$i->wait( 1 );
+		$i->click( 'button[data-testid="edit-model-update-create-button"]' );
+		$i->wait( 1 );
+
+		$i->amOnPage( '/wp-admin/edit.php?post_type=candy' );
+		$i->click( 'Add New', '.wrap' );
+		$i->wait( 1 );
+
+		$i->fillField( [ 'name' => 'atlas-content-modeler[candy][colors][0]' ], 'red' );
+		$i->click( 'Publish', '#publishing-action' );
+		$i->wait( 1 );
+		$i->seeElement( 'input[name="atlas-content-modeler[candy][colors][1]"]:invalid' );
+	}
+
+	public function a_non_required_repeatable_text_field_with_a_minimum_will_not_trigger_validation_if_minimum_valid_fields_are_present( AcceptanceTester $i ) {
+		$i->click( 'button[data-testid="edit-model-update-create-settings-button"]' );
+		$i->fillField( [ 'name' => 'minRepeatable' ], '2' );
+		$i->click( 'button[data-testid="model-advanced-settings-done-button"]' );
+		$i->wait( 1 );
+		$i->click( 'button[data-testid="edit-model-update-create-button"]' );
+		$i->wait( 1 );
+
+		$i->amOnPage( '/wp-admin/edit.php?post_type=candy' );
+		$i->click( 'Add New', '.wrap' );
+		$i->wait( 1 );
+
+		$i->fillField( [ 'name' => 'atlas-content-modeler[candy][colors][0]' ], 'red' );
+		$i->fillField( [ 'name' => 'atlas-content-modeler[candy][colors][1]' ], 'blue' );
+		$i->click( 'Publish', '#publishing-action' );
+		$i->wait( 1 );
+		$i->see( 'Post published' );
+	}
+
+	public function a_non_required_repeatable_text_field_with_the_same_minimum_and_maximum_will_trigger_validation_if_less_than_minimum_fields_are_present( AcceptanceTester $i ) {
+		$i->click( 'button[data-testid="edit-model-update-create-settings-button"]' );
+		$i->fillField( [ 'name' => 'minRepeatable' ], '2' );
+		$i->fillField( [ 'name' => 'maxRepeatable' ], '2' );
+		$i->click( 'button[data-testid="model-advanced-settings-done-button"]' );
+		$i->wait( 1 );
+		$i->click( 'button[data-testid="edit-model-update-create-button"]' );
+		$i->wait( 1 );
+
+		$i->amOnPage( '/wp-admin/edit.php?post_type=candy' );
+		$i->click( 'Add New', '.wrap' );
+		$i->wait( 1 );
+
+		$i->fillField( [ 'name' => 'atlas-content-modeler[candy][colors][0]' ], 'red' );
+		$i->click( 'Publish', '#publishing-action' );
+		$i->wait( 1 );
+		$i->seeElement( 'input[name="atlas-content-modeler[candy][colors][1]"]:invalid' );
+	}
+
+	public function a_repeatable_text_field_with_a_maximum_of_three_will_only_allow_a_max_of_three_fields( AcceptanceTester $i ) {
 		$i->click( 'button[data-testid="edit-model-update-create-settings-button"]' );
 		$i->fillField( [ 'name' => 'maxRepeatable' ], '3' );
 		$i->click( 'button[data-testid="model-advanced-settings-done-button"]' );
