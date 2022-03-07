e5adb042e630ce63eb81831fcfce37b1bae115a5
John Parris <john.parris@wpengine.com>
2022-03-07 15:59:29 -0500

    test: confirm title field value is also available under field slug name.

--- tests/integration/api-validation/test-graphql-model-data.php
+++ tests/integration/api-validation/test-graphql-model-data.php
@@ -59,6 +59,7 @@ class GraphQLModelDataTests extends WP_UnitTestCase {
 						nodes {
 							databaseId
 							title
+							singleLineRequired
 							richText
 							numberIntergerRequired
 							numberIntegerRepeat
@@ -91,9 +92,11 @@ class GraphQLModelDataTests extends WP_UnitTestCase {
 			);
 
 			self::assertArrayHasKey( 'databaseId', $results['data']['publicsFields']['nodes'][0] );
-
 			self::assertArrayHasKey( 'title', $results['data']['publicsFields']['nodes'][0] );
 			self::assertSame( $results['data']['publicsFields']['nodes'][0]['title'], 'Test dog with fields' );
+
+			self::assertArrayHasKey( 'singleLineRequired', $results['data']['publicsFields']['nodes'][0] );
+			self::assertSame( $results['data']['publicsFields']['nodes'][0]['singleLineRequired'], 'Test dog with fields' );
 
 			self::assertArrayHasKey( 'richText', $results['data']['publicsFields']['nodes'][0] );
 			self::assertSame( $results['data']['publicsFields']['nodes'][0]['richText'], 'This is a rich text field' );
