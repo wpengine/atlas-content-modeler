936a7541f53d3fe35d12ebb731e1c60c174d16df
Andrew Botz <andrew.botz@getflywheel.com>
2022-03-08 09:28:30 -0600

    chore: Add additional colors to theme

--- includes/shared-assets/js/theme/colors.js
+++ includes/shared-assets/js/theme/colors.js
@@ -1,3 +1,4 @@
+// TODO-abotz: Need to verify colors and possible restructure object if makes more sense to do so
 export default {
 	white: "#fff",
 	text: "#002838",
@@ -6,4 +7,9 @@ export default {
 	warning: "#d21b46",
 	warningHover: "#991433",
 	grayLight: "#9db7d1",
+	primary: "#7e5cef",
+	primaryHover: "#5c43ae",
+	grayDisabledText: "#59767f",
+	blueDark: "#002838",
+	blueDarkHover: "#004c6b",
 };
