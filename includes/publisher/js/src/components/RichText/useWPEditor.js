1c504d473bf6095a3180a71118b846bf000b13f8
Nick Cernis <nick@cern.is>
2022-03-17 16:33:22 +0100

    design: bump height a little
    Mason wants three lines to be visible.

--- includes/publisher/js/src/components/RichText/useWPEditor.js
+++ includes/publisher/js/src/components/RichText/useWPEditor.js
@@ -14,7 +14,7 @@ export default function useWpEditor(textareaIds) {
 		const editorReadyTime = 500;
 		const editorSettingsOverrides = {
 			tinymce: {
-				height: "120",
+				height: "125",
 				toolbar1:
 					"undo,redo,formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media",
 			},
