9d51964275aeb07152cc8c0daf5328bb750a344b
Andrew Botz <andrew.botz@getflywheel.com>
2022-03-17 10:28:38 -0500

    Remove repeatable logic from EmailFields

--- includes/settings/js/src/components/fields/EmailFields.jsx
+++ includes/settings/js/src/components/fields/EmailFields.jsx
@@ -21,33 +21,6 @@ const EmailFields = ({ register, data, editing, fieldId }) => {
 
 	return (
 		<>
-			{data && (
-				<div
-					className={showTitle ? "field read-only editing" : "field"}
-				>
-					<legend>
-						{__("Repeatable Field", "atlas-content-modeler")}
-					</legend>
-					<input
-						name="isRepeatable"
-						type="checkbox"
-						id={`is-repeatable-${fieldId}`}
-						ref={register}
-						defaultChecked={showRepeatable}
-						onChange={() => setShowRepeatable(!showRepeatable)}
-						disabled={editing || showTitle}
-					/>
-					<label
-						htmlFor={`is-repeatable-${fieldId}`}
-						className="checkbox is-repeatable"
-					>
-						{__(
-							"Make this field repeatable",
-							"atlas-content-modeler"
-						)}
-					</label>
-				</div>
-			)}
 			{showTitleField && (
 				<div
 					className={
