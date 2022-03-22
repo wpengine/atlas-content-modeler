3dd828d78d7aa6494a5b43cbfcdb082241a8cf91
Anthony Burchell <anthony.burchell@wpengine.com>
2022-03-22 10:35:54 -0500

    adds array filter for repeating dates

--- includes/publisher/lib/field-functions.php
+++ includes/publisher/lib/field-functions.php
@@ -214,6 +214,20 @@ function sanitize_field( string $type, $value ) {
 			return filter_var( $value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
 		case 'date':
 			$y_m_d_format = '/\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])/';
+
+			if ( is_array( $value ) ) {
+				return array_filter(
+					array_map(
+						function( $val ) use ( $y_m_d_format ) {
+							if ( preg_match( $y_m_d_format, $val ) ) {
+								return $val;
+							}
+							return [ '' ];
+						},
+						$value
+					)
+				);
+			}
 			if ( preg_match( $y_m_d_format, $value ) ) {
 				return $value;
 			}
