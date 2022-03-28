6735c2a6ea3a9179c23ec351122431a13ede7483
Ryan Meier <ryan.meier@wpengine.com>
2022-03-28 13:29:47 -0500

    Added api crud functions file and namespsace

---
+++ includes/api/crud-functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Functions to create and modify content model entries.
+ *
+ * @package AtlasContentModeler
+ */
+
+declare(strict_types=1);
+
+namespace WPE\AtlasContentModeler\API;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
