623ce7a4c767d620a863dd5105c30a42c65df95c
Ryan Meier <ryan.meier@wpengine.com>
2022-03-30 16:02:51 -0500

    Added remaining tests for possible return values for replace_relationship

--- includes/api/crud-functions.php
+++ includes/api/crud-functions.php
@@ -30,18 +30,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 function replace_relationship( int $post_id, string $relation_field_slug, array $relation_ids ) {
 	$post = get_post( $post_id );
 	if ( empty( $post ) ) {
-		return new \WP_Error();
+		return new \WP_Error( 'invalid_post_object', 'The post object was invalid' );
 	}
 
 	$field = get_field_from_slug( $relation_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
 	if ( empty( $field ) ) {
-		return new \WP_Error();
+		return new \WP_Error( 'field_not_found', 'Content model field not found' );
 	}
 
 	$registry     = ContentConnect::instance()->get_registry();
 	$relationship = $registry->get_post_to_post_relationship( $post->post_type, $field['reference'], $field['id'] );
 	if ( ! $relationship ) {
-		return new \WP_Error();
+		return new \WP_Error( 'content_relationship_not_found', 'Content model relationship not found' );
 	}
 
 	return $relationship->replace_relationships( $post->ID, $relation_ids );
--- tests/integration/api/test-crud-functions.php
+++ tests/integration/api/test-crud-functions.php
@@ -21,7 +21,6 @@ class TestApiFunctions extends Integration_TestCase {
 		parent::set_up();
 
 		$this->content_models = $this->get_models( __DIR__ . '/test-data/content-models.php' );
-
 		update_registered_content_types( $this->content_models );
 		Plugin::instance()->setup();
 		do_action( 'init' );
@@ -33,11 +32,50 @@ class TestApiFunctions extends Integration_TestCase {
 		$post_id      = $this->factory->post->create( [ 'post_type' => 'person' ] );
 		$relation_ids = $this->factory->post->create_many( 3, [ 'post_type' => 'car' ] );
 
-		replace_relationship( $post_id, 'cars', $relation_ids );
+		$this->assertTrue( replace_relationship( $post_id, 'cars', $relation_ids ) );
 
 		$result = $wpdb->get_results( "SELECT id2 FROM {$wpdb->prefix}acm_post_to_post WHERE id1 IN( {$post_id} );" );
 		$result = array_map( 'intval', wp_list_pluck( $result, 'id2' ) );
 
 		$this->assertEquals( $relation_ids, $result );
+	}
+
+	public function test_replace_relationship_will_return_WP_Error_if_invalid_post() {
+		$result = replace_relationship( 999, 'cars', [] );
+
+		$this->assertEquals( 'invalid_post_object', $result->get_error_code() );
+		$this->assertEquals( 'The post object was invalid', $result->get_error_message() );
+	}
+
+	public function test_replace_relationship_will_return_WP_Error_if_invalid_field() {
+		$post_id = $this->factory->post->create( [ 'post_type' => 'person' ] );
+		$result  = replace_relationship( $post_id, 'does_not_exist', [] );
+
+		$this->assertEquals( 'field_not_found', $result->get_error_code() );
+		$this->assertEquals( 'Content model field not found', $result->get_error_message() );
+	}
+
+	public function test_replace_relationship_will_return_WP_Error_if_invalid_content_model_relationship() {
+		$this->content_models['person']['fields']['1648576059444']['reference'] = 'does_not_exist';
+		update_option( 'atlas_content_modeler_post_types', $this->content_models );
+
+		$post_id      = $this->factory->post->create( [ 'post_type' => 'person' ] );
+		$relation_ids = $this->factory->post->create_many( 3, [ 'post_type' => 'car' ] );
+		$result       = replace_relationship( $post_id, 'cars', $relation_ids );
+
+		$this->assertEquals( 'content_relationship_not_found', $result->get_error_code() );
+		$this->assertEquals( 'Content model relationship not found', $result->get_error_message() );
+	}
+
+	public function test_replace_relationship_will_return_false_if_relationship_could_not_be_associated() {
+		$this->content_models['person']['fields']['1648576059444']['cardinality'] = 'one-to-one';
+		update_registered_content_types( $this->content_models );
+		Plugin::instance()->setup();
+		do_action( 'init' );
+
+		$post_id      = $this->factory->post->create( [ 'post_type' => 'person' ] );
+		$relation_ids = $this->factory->post->create_many( 3, [ 'post_type' => 'car' ] );
+
+		$this->assertFalse( replace_relationship( $post_id, 'cars', $relation_ids ) );
 	}
 }
