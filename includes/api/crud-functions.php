f9bac4a06478bef30eee21e768df907521c387d8
Ryan Meier <ryan.meier@wpengine.com>
2022-03-31 12:27:51 -0500

    Added add_relationship function to api

--- includes/api/crud-functions.php
+++ includes/api/crud-functions.php
@@ -17,6 +17,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Add a relationship to a given post.
+ *
+ * Relationship must already be defined between models.
+ *
+ * @param int    $post_id The post or content entry id.
+ * @param string $relation_field_slug The content model field slug.
+ * @param int    $relationship_id The post id to associate.
+ *
+ * @return bool|WP_Error False or WP_Error if relation could not be made, else true.
+ */
+function add_relationship( int $post_id, string $relation_field_slug, int $relationship_id ) {
+	$post = get_post( $post_id );
+	if ( empty( $post ) ) {
+		return new \WP_Error( 'invalid_post_object', 'The post object was invalid' );
+	}
+
+	$field = get_field_from_slug( $relation_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
+	if ( empty( $field ) ) {
+		return new \WP_Error( 'field_not_found', 'Content model field not found' );
+	}
+
+	$registry     = ContentConnect::instance()->get_registry();
+	$relationship = $registry->get_post_to_post_relationship( $post->post_type, $field['reference'], $field['id'] );
+	if ( ! $relationship ) {
+		return new \WP_Error( 'content_relationship_not_found', 'Content model relationship not found' );
+	}
+
+	return $relationship->add_relationship( $post->ID, $relationship_id );
+}
+
+/**
  * Replace relationships between posts.
  *
  * Relationship must already be defined between models.
