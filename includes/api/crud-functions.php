bcc946aa6502470095b47f61352a887682d38303
Ryan Meier <ryan.meier@wpengine.com>
2022-03-31 12:38:39 -0500

    Renamed relation_ variables to relationship_

--- includes/api/crud-functions.php
+++ includes/api/crud-functions.php
@@ -22,18 +22,18 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Relationship must already be defined between models.
  *
  * @param int    $post_id The post or content entry id.
- * @param string $relation_field_slug The content model field slug.
+ * @param string $relationship_field_slug The content model field slug.
  * @param int    $relationship_id The post id to associate.
  *
  * @return bool|WP_Error False or WP_Error if relation could not be made, else true.
  */
-function add_relationship( int $post_id, string $relation_field_slug, int $relationship_id ) {
+function add_relationship( int $post_id, string $relationship_field_slug, int $relationship_id ) {
 	$post = get_post( $post_id );
 	if ( empty( $post ) ) {
 		return new \WP_Error( 'invalid_post_object', 'The post object was invalid' );
 	}
 
-	$field = get_field_from_slug( $relation_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
+	$field = get_field_from_slug( $relationship_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
 	if ( empty( $field ) ) {
 		return new \WP_Error( 'field_not_found', 'Content model field not found' );
 	}
@@ -53,18 +53,18 @@ function add_relationship( int $post_id, string $relation_field_slug, int $relat
  * Relationship must already be defined between models.
  *
  * @param int    $post_id The post or content entry id.
- * @param string $relation_field_slug The content model field slug.
- * @param array  $relation_ids Array of post or content entry ids.
+ * @param string $relationship_field_slug The content model field slug.
+ * @param array  $relationship_ids Array of post or content entry ids.
  *
  * @return bool|WP_Error False or WP_Error if relation could not be made, else true.
  */
-function replace_relationship( int $post_id, string $relation_field_slug, array $relation_ids ) {
+function replace_relationship( int $post_id, string $relationship_field_slug, array $relationship_ids ) {
 	$post = get_post( $post_id );
 	if ( empty( $post ) ) {
 		return new \WP_Error( 'invalid_post_object', 'The post object was invalid' );
 	}
 
-	$field = get_field_from_slug( $relation_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
+	$field = get_field_from_slug( $relationship_field_slug, get_option( 'atlas_content_modeler_post_types' ), $post->post_type );
 	if ( empty( $field ) ) {
 		return new \WP_Error( 'field_not_found', 'Content model field not found' );
 	}
@@ -75,5 +75,5 @@ function replace_relationship( int $post_id, string $relation_field_slug, array 
 		return new \WP_Error( 'content_relationship_not_found', 'Content model relationship not found' );
 	}
 
-	return $relationship->replace_relationships( $post->ID, $relation_ids );
+	return $relationship->replace_relationships( $post->ID, $relationship_ids );
 }
