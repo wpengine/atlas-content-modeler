10bcfef93c00c2e52d2cc4a746fa364c8fabab59
Nick Cernis <nick@cern.is>
2022-04-04 13:09:49 +0200

    refactor: extract form submit logic

--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -8,6 +8,22 @@ import "../../../settings/scss/index.scss";
 const { models, postType } = atlasContentModelerFormEditingExperience;
 const container = document.getElementById("atlas-content-modeler-fields-app");
 
+/**
+ * Adds a "submitted" class when the publish form is submitted.
+ *
+ * Allows styling of :invalid input fields only when the form was
+ * submitted. Prevents an issue where error messages appear for
+ * required fields when the form is first loaded.
+ */
+const addClassOnFormSubmit = () => {
+	const form = document.querySelector("form#post");
+	const publishButton = document.querySelector("input#publish");
+	const addSubmittedClass = () => form.classList.add("submitted");
+
+	publishButton.addEventListener("click", addSubmittedClass);
+	form.addEventListener("submit", addSubmittedClass);
+};
+
 if (container && models.hasOwnProperty(postType)) {
 	const model = models[postType];
 	const urlParams = new URLSearchParams(window.location.search);
@@ -17,15 +33,5 @@ if (container && models.hasOwnProperty(postType)) {
 		container
 	);
 
-	/**
-	 * Allows styling of :invalid input fields only when the form was
-	 * submitted. Prevents an issue where error messages appear for
-	 * required fields when the form is first loaded.
-	 */
-	const form = document.querySelector("form#post");
-	const publishButton = document.querySelector("input#publish");
-	const addSubmittedClass = () => form.classList.add("submitted");
-
-	publishButton.addEventListener("click", addSubmittedClass);
-	form.addEventListener("submit", addSubmittedClass);
+	addClassOnFormSubmit();
 }
