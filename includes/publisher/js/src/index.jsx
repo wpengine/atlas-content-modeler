56c56445132ffe696f89df36c52e910a0d5e4315
Nick Cernis <nick@cern.is>
2022-04-04 16:32:35 +0200

    feature: focus the first field on new post screens

--- includes/publisher/js/src/index.jsx
+++ includes/publisher/js/src/index.jsx
@@ -4,25 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./../../scss/index.scss";
 import "../../../settings/scss/index.scss";
+import { addClassOnFormSubmit, focusFirstField } from "./publisherFormActions";
 
 const { models, postType } = atlasContentModelerFormEditingExperience;
 const container = document.getElementById("atlas-content-modeler-fields-app");
-
-/**
- * Adds a "submitted" class when the publish form is submitted.
- *
- * Allows styling of :invalid input fields only when the form was
- * submitted. Prevents an issue where error messages appear for
- * required fields when the form is first loaded.
- */
-const addClassOnFormSubmit = () => {
-	const form = document.querySelector("form#post");
-	const publishButton = document.querySelector("input#publish");
-	const addSubmittedClass = () => form.classList.add("submitted");
-
-	publishButton.addEventListener("click", addSubmittedClass);
-	form.addEventListener("submit", addSubmittedClass);
-};
 
 if (container && models.hasOwnProperty(postType)) {
 	const model = models[postType];
@@ -34,4 +19,5 @@ if (container && models.hasOwnProperty(postType)) {
 	);
 
 	addClassOnFormSubmit();
+	focusFirstField();
 }
