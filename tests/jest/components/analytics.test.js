58bedc3f3246c31786a34b430781270a77b785ed
Matthew Wright <matthew.wright@wpengine.com>
2022-04-04 11:30:34 -0500

    unit tests for meta

--- tests/jest/components/analytics.test.js
+++ tests/jest/components/analytics.test.js
@@ -1,13 +1,89 @@
 import {
+	isExcluded,
 	sendEvent,
 	sendPageView,
-	isExcluded,
 	shouldTrack,
 	telemetryEnabled,
 } from "../../../includes/shared-assets/js/analytics";
 
-test("telemetry", () => {});
+describe("telemetry", () => {
+	test("telemetry should return true", () => {
+		window.atlasContentModelerFormEditingExperience = {
+			usageTrackingEnabled: true,
+		};
+		window.atlasContentModeler = { usageTrackingEnabled: true };
+
+		expect(telemetryEnabled()).toBeTruthy();
+	});
+
+	test("telemetry should return false", () => {
+		window.atlasContentModelerFormEditingExperience = {
+			usageTrackingEnabled: false,
+		};
+		window.atlasContentModeler = { usageTrackingEnabled: false };
+
+		expect(telemetryEnabled()).toBeFalsy();
+	});
+});
 
 describe("analytics", () => {
-	test("", () => {});
+	test("isExcluded should return true", () => {
+		global.window = Object.create(window);
+		Object.defineProperty(window, "location", {
+			value: {
+				href: "http://www.wpengine.com",
+			},
+		});
+
+		expect(isExcluded()).toBeTruthy();
+	});
+	test("isExcluded should return false", () => {
+		global.window = Object.create(window);
+		Object.defineProperty(window, "location", {
+			value: {
+				href: "http://www.google.com",
+			},
+		});
+
+		expect(isExcluded()).toBeFalsy();
+	});
+
+	test("shouldTrack should return true if the url does not contain wpengine.com", () => {
+		global.window = Object.create(window);
+		Object.defineProperty(window, "location", {
+			value: {
+				href: "http://www.google.com",
+			},
+		});
+
+		window.atlasContentModelerFormEditingExperience = {
+			usageTrackingEnabled: true,
+		};
+		window.atlasContentModeler = { usageTrackingEnabled: true };
+
+		expect(shouldTrack()).toBeTruthy();
+	});
+
+	test("shouldTrack should return false if the url contains wpengine.com", () => {
+		global.window = Object.create(window);
+		Object.defineProperty(window, "location", {
+			value: {
+				href: "http://www.wpengine.com",
+			},
+		});
+
+		window.atlasContentModelerFormEditingExperience = {
+			usageTrackingEnabled: true,
+		};
+		window.atlasContentModeler = { usageTrackingEnabled: true };
+
+		expect(shouldTrack()).toBeFalsy();
+	});
+
+	test("sendEvent should call shouldTrack()", () => {
+		global.shouldTrack = jest.fn();
+		const shouldTrackSpy = jest.spyOn(global, "shouldTrack");
+		sendEvent();
+		expect(shouldTrackSpy).toHaveBeenCalled();
+	});
 });
