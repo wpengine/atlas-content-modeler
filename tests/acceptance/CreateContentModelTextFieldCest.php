548f2a0e630f7be815eac8c8f5953653889693d5
Nick Cernis <nick@cern.is>
2022-04-07 17:18:59 +0200

    MTKA-1463: Focus newly added text repeater field rows (#490)
    * fix: focus last text repeater field when added
    
    * refactor: make enter move to next field
    
    Unless focus is in the final field, when a new field will be
    added.
    
    * refactor: remove uneeded anonymous function
    
    * test: new repeating text inputs gain focus
    
    Co-authored-by: Matthew Wright <1815200+matthewguywright@users.noreply.github.com>

--- tests/acceptance/CreateContentModelTextFieldCest.php
+++ tests/acceptance/CreateContentModelTextFieldCest.php
@@ -56,6 +56,11 @@ class CreateContentModelTextFieldCest {
 		// The first text field should be in focus as the first field on the page.
 		$active_element = $i->executeJS( "return document.activeElement.getAttribute('name');" );
 		$i->assertEquals( 'atlas-content-modeler[candy][color][0]', $active_element );
+    
+    // Confirm new repeating text inputs gain focus when they are added.
+		$i->click( 'Add Item', '.add-option' );
+		$active_element = $i->executeJS( "return document.activeElement.getAttribute('name');" );
+		$i->assertEquals( 'atlas-content-modeler[candy][color][1]', $active_element );
 	}
 
 	public function i_can_create_a_content_model_text_field_as_a_textarea( AcceptanceTester $i ) {
