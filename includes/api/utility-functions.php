8242686c0fc639dc9909598a2a6bd058715205c4
Ryan Meier <ryan.meier@wpengine.com>
2022-05-19 15:24:46 -0500

    Added array_remove_by_keys() to utility functions

--- includes/api/utility-functions.php
+++ includes/api/utility-functions.php
@@ -23,3 +23,18 @@ function array_extract_by_keys( array $array, array $keys ): array {
 		array_flip( $keys )
 	);
 }
+
+/**
+ * Create a new array with the given keys values removed.
+ *
+ * @param array<int|string,mixed> $array Associative array of data.
+ * @param array<int,int|string>   $keys  Array of keys to remove.
+ *
+ * @return array Array with key values removed.
+ */
+function array_remove_by_keys( array $array, array $keys ): array {
+	return \array_diff_key(
+		$array,
+		\array_flip( $keys )
+	);
+}
--- tests/integration/api/test-utility-functions.php
+++ tests/integration/api/test-utility-functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use function WPE\AtlasContentModeler\API\array_extract_by_keys;
+use function WPE\AtlasContentModeler\API\array_remove_by_keys;
 
 class TestUtilityFunctions extends WP_UnitTestCase {
 	public function test_array_extract_by_keys_will_extract_assoc_keys_as_array_given_keys() {
@@ -27,5 +28,33 @@ class TestUtilityFunctions extends WP_UnitTestCase {
 		);
 
 		$this->assertEquals( [], array_extract_by_keys( $data, [ 'does_not_exist' ] ) );
+	}
+
+	public function test_array_remove_by_keys_will_return_an_array_without_given_keys() {
+		$data = [
+			'first' => 2,
+			'last'  => 3,
+			3       => 'some value',
+		];
+
+		$this->assertEquals(
+			[ 'first' => 2 ],
+			array_remove_by_keys( $data, [ 'last', 3 ] )
+		);
+
+		$this->assertEquals(
+			$data,
+			array_remove_by_keys( $data, [ 'does_not_exist' ] )
+		);
+
+		$this->assertEquals(
+			$data,
+			array_remove_by_keys( $data, [] )
+		);
+
+		$this->assertEquals(
+			[],
+			array_remove_by_keys( $data, [ 'first', 'last', 3 ] )
+		);
 	}
 }
