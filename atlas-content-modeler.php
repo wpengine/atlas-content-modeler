0cc64e891bca90282706ffd5814ed93bbd111b4f
Nick Cernis <nick@cern.is>
2022-07-18 15:54:46 +0200

    feature: add ACM reset WP-CLI command

--- atlas-content-modeler.php
+++ atlas-content-modeler.php
@@ -67,7 +67,9 @@ function atlas_content_modeler_loader(): void {
 
 	if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		include_once ATLAS_CONTENT_MODELER_INCLUDES_DIR . '/wp-cli/class-blueprint.php';
+		include_once ATLAS_CONTENT_MODELER_INCLUDES_DIR . '/wp-cli/class-reset.php';
 		\WP_CLI::add_command( 'acm blueprint', 'WPE\AtlasContentModeler\WP_CLI\Blueprint' );
+		\WP_CLI::add_command( 'acm reset', new WPE\AtlasContentModeler\WP_CLI\Reset() );
 	}
 
 	\WPE\AtlasContentModeler\VersionUpdater\update_plugin();
