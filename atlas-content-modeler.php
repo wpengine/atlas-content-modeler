98080f21c7d542af5c94b118aeedd36bec384079
John Parris <john.parris@wpengine.com>
2022-09-06 09:16:31 -0400

    chore: prepare 0.21.0 release

--- atlas-content-modeler.php
+++ atlas-content-modeler.php
@@ -7,7 +7,7 @@
  * Author URI: https://wpengine.com/
  * Text Domain: atlas-content-modeler
  * Domain Path: /languages
- * Version: 0.20.0
+ * Version: 0.21.0
  * Requires at least: 5.7
  * Requires PHP: 7.2
  * License: GPLv2 or later
