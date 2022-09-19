3c77fdc1d69af2a60d1ab004e4e71c31959f3d8a
Nick Cernis <nick@cern.is>
2022-09-19 09:41:49 +0200

    test: check model ID failure paths

---
+++ tests/integration/wp-cli/test-model-change-id.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Tests model ID migration.
+ *
+ * @package AtlasContentModeler
+ */
+
+use function WPE\AtlasContentModeler\ContentRegistration\update_registered_content_types;
+
+require_once ATLAS_CONTENT_MODELER_INCLUDES_DIR . '/wp-cli/class-model.php';
+
+/**
+ * Class ModelChangeIdTest
+ *
+ * @covers WPE\AtlasContentModeler\WP_CLI\Model
+ */
+class ModelChangeIdTest extends WP_UnitTestCase {
+	private $model;
+
+	public function set_up() {
+		parent::set_up();
+
+		$models = [
+			'old-id' => [
+				'slug'     => 'old-id',
+				'singular' => 'Old ID',
+				'plural'   => 'Old IDs',
+				'fields'   => [],
+			],
+		];
+
+		update_registered_content_types( $models );
+
+		$this->model = new \WPE\AtlasContentModeler\WP_CLI\Model();
+	}
+
+	public function test_model_id_is_not_updated_if_old_id_not_in_model_list() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'No model exists with the old ID of ‘bad-old-id’.' );
+
+		$this->model->change_id( [ 'bad-old-id', 'new-id' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_is_longer_than_20_chars() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New model ID must not exceed 20 characters.' );
+
+		$this->model->change_id( [ 'old-id', 'bad-new-id-that-is-longer-than-20-characters' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_starts_with_number() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New model ID must not start with a number.' );
+
+		$this->model->change_id( [ 'old-id', '7bad-new-id' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_has_invalid_characters() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New model ID must only contain lowercase alphanumeric characters, underscores and hyphens.' );
+
+		$this->model->change_id( [ 'old-id', 'bad-new-id-@!!!' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_is_in_use_by_another_model() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New ID of ‘old-id’ is in use by another model.' );
+
+		$this->model->change_id( [ 'old-id', 'old-id' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_is_a_reserved_slug() {
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New ID of ‘theme’ is reserved or in use by WordPress Core.' );
+
+		$this->model->change_id( [ 'old-id', 'theme' ] );
+	}
+
+	public function test_model_id_is_not_updated_if_new_id_is_in_use_by_a_custom_post_type() {
+		register_post_type( 'custom-post-type', [] );
+		$this->model = new \WPE\AtlasContentModeler\WP_CLI\Model();
+
+		$this->expectException( \Exception::class );
+		$this->expectExceptionMessage( 'New ID of ‘custom-post-type’ is in use by a custom post type.' );
+
+		$this->model->change_id( [ 'old-id', 'custom-post-type' ] );
+	}
+}
