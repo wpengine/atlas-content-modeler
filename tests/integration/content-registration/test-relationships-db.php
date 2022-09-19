a86888bbb1786eecbf443c2401083614edc57406
Nick Cernis <nick@cern.is>
2022-09-19 20:07:28 +0200

    test: do not drop post_to_post table
    This causes 'table missing' test failures in subsequent tests that depend on
    this table's existence.

--- tests/integration/content-registration/test-relationships-db.php
+++ tests/integration/content-registration/test-relationships-db.php
@@ -2,13 +2,6 @@
 use WPE\AtlasContentModeler\ContentConnect\Tables\PostToPost;
 
 class TestRelationshipsDB extends WP_UnitTestCase {
-	public function tear_down(): void {
-		global $wpdb;
-		parent::tear_down();
-		$table = $wpdb->prefix . 'acm_post_to_post';
-		$wpdb->query( "DROP TABLE IF EXISTS $table" );
-	}
-
 	public function test_relationships_db_exists(): void {
 		/**
 		 * @var \wpdb $wpdb
