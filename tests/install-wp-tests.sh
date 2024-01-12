376a0bc59767a67302ebfdb64f0bdd113c673e50
John Parris <john.parris@wpengine.com>
2024-01-12 10:11:46 -0500

    ci: enable curl redirect

--- tests/install-wp-tests.sh
+++ tests/install-wp-tests.sh
@@ -19,7 +19,7 @@ WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
 download() {
     if [ `which curl` ]; then
-        curl -s "$1" > "$2";
+        curl -L -s "$1" > "$2";
     elif [ `which wget` ]; then
         wget -nv -O "$2" "$1"
     fi
